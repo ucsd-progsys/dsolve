884d15bd293f8b828fa19856c9e793d3af8fec05
prondon <prondon@17133952-631a-0410-a48c-e94f9446eeac>
2007-02-26 19:03:31 +0000

    Modify the in
    git-svn-id: https://pho.ucsd.edu/svn/papers/PreQual/trunk@467 17133952-631a-0410-a48c-e94f9446eeac

--- src/test/test_flowgraph.ml
+++ src/test/test_flowgraph.ml
@@ -16,10 +16,28 @@ let test_regular_flow _ =
   let graph = create_simple_graph v1 None v2 in
   let v1_qset = LabelledQualSet.singleton (QualFrom("Q", None)) in
   let qmap = QualMap.add v1 v1_qset QualMap.empty in
-  let result = propagate_vertex_qualifiers graph qmap [v1] in
+  let result = propagate_vertex_qualifiers graph qmap [v2] in
     assert_bool "Did not propagate qualifier across simple edge"
       (LabelledQualSet.equal v1_qset (QualMap.find v2 result))
 
 
+let test_cycle_termination _ =
+  let v1 = FlowGraph.V.create "v1" in
+  let v2 = FlowGraph.V.create "v2" in
+  let forward_edge = FlowGraph.E.create v1 None v2 in
+  let backward_edge = FlowGraph.E.create v2 None v1 in
+  let graph = FlowGraph.empty in
+  let vgraph = FlowGraph.add_vertex (FlowGraph.add_vertex graph v1) v2 in
+  let egraph = FlowGraph.add_edge_e (FlowGraph.add_edge_e vgraph forward_edge)
+    backward_edge
+  in
+  let v1_qset = LabelledQualSet.singleton (QualFrom("Q", None)) in
+  let qmap = QualMap.add v1 v1_qset QualMap.empty in
+  let result = propagate_vertex_qualifiers egraph qmap [v2] in
+    assert_bool "Terminated without propagating qualifier in cycle"
+      (LabelledQualSet.equal v1_qset (QualMap.find v2 result))
+
+
 let suite = "Test Flowgraph" >:::
-  ["test_regular_flow" >:: test_regular_flow]
+  ["test_regular_flow" >:: test_regular_flow;
+   "test_cycle_termination" >:: test_cycle_termination]
