e2835929ea37c813deda0dfe6aa6f85d2cfce42c
Pat Rondon <pmr@famine.(none)>
2007-07-12 22:16:00 -0700

    Add a small "repeat" example leading up to dot

---
+++ src/small/smallrep.ml
@@ -0,0 +1,13 @@
+qual LEN(x): x <= n;;
+qual LN(x): x < n;;
+
+? letrec foo = fun n -> fun i -> fun f -> fun c ->
+  if i < n then
+    let d = f i c in
+    let k = i + 1 in
+      foo n k f d
+  else
+    i
+in
+let g = fun s -> fun t -> s + t in
+  foo 10 0 g 0;;
