3c081b9a25760cff9b59a0351dff91fd485865f9
Pat Rondon <pmr@famine.(none)>
2007-07-13 15:06:04 -0700

    Need env to build =P

---
+++ src/env.ml
@@ -0,0 +1,15 @@
+module E = Map.Make(String)
+
+include E
+
+let maplist f env =
+  fold (fun k v r -> (f k v)::r) env []
+
+let filterlist f env =
+  fold (fun k v r -> if f k v then v::r else r) env []
+
+let mapfilter f env =
+  fold (fun k v r -> match f k v with Some x -> x::r | None -> r) env []
+
+let addn items env =
+  List.fold_left (fun e (k, v) -> add k v e) env items
