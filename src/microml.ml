a885e7b2793fada6ce4f39297acdce9eebe53866
Pat Rondon <pmr@meatwad.(none)>
2007-08-02 13:51:04 -0700

    Print basic type info again; probably still b0rked

--- src/microml.ml
+++ src/microml.ml
@@ -1,4 +1,23 @@
+open Expr
+open Frame
+open Parsetree
+open Infer
+
+
+let type_expression exp =
+  let framemap = infer_frames exp Env.empty in
+  let annotate e = pprint_frame (ExpMap.find e framemap) in
+    pprint_annotated_expression annotate 0 exp
+
+
 exception PPerror
+
+let eval_structure_item si =
+  match si.pstr_desc with
+      Pstr_eval (e) ->
+	type_expression e
+    | _ -> raise PPerror
+
 
 let main () =
   let lb = Lexing.from_channel stdin in
@@ -9,7 +28,11 @@ let main () =
       Lexing.flush_input lb;
       Location.reset();
       let phr = try Parse.toplevel_phrase lb with Exit -> raise PPerror in
-      ignore(Printast.top_phrase Format.std_formatter phr)
+	match phr with
+	    Ptop_def (is) ->
+	      Printf.printf "%s\n" (Misc.join (List.map eval_structure_item is) "\n")
+	  | _ -> raise PPerror
+
     with
     | End_of_file -> exit 0
     | PPerror -> ()
