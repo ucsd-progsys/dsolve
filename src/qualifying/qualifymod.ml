5b50cea32e9acf9f7f04dd5042fedfc525e18be4
Pat Rondon <pmr@famine.(none)>
2007-09-24 14:50:39 -0700

    Sketch in support for ifs (handling of type environemnts is ugly and will need to be fixed, I think)

--- src/qualifying/qualifymod.ml
+++ src/qualifying/qualifymod.ml
@@ -5,6 +5,7 @@ open Types
 open Frame
 open Predicate
 open Constraint
+open Longident
 
 module LocationMap = Map.Make(struct type t = Location.t
                                      let compare = compare end)
@@ -12,7 +13,7 @@ module LocationMap = Map.Make(struct type t = Location.t
 (* pmr: must remove redundant definitions of maplist *)
 let maplist f sm = LocationMap.fold (fun k v r -> (f k v)::r) sm []
 
-let constrain_expression exp cstrs initframemap =
+let constrain_expression tenv exp cstrs initframemap =
   let rec constrain e env guard constrs framemap =
     let (f, cs, fm) =
       match (e.exp_desc, (repr e.exp_type).desc) with
@@ -23,8 +24,28 @@ let constrain_expression exp cstrs initframemap =
             let cstrref =
               match cstrdesc.cstr_tag with
                   Cstr_constant n -> Builtins.equality_refinement (PInt n)
-                | _ -> Builtins.true_refinement
+                | _ -> Builtins.empty_refinement
             in (ref (Fconstr(path, [], cstrref)), constrs, framemap)
+	| (Texp_ifthenelse(e1, e2, Some e3), _) ->
+            let f = fresh e.exp_type in
+            let (f1, cstrs1, fm1) = constrain e1 env guard cstrs framemap in
+            let guardvar = Ident.create "guard" in
+            let true_tag =
+              match (Env.lookup_constructor (Lident "true") tenv).cstr_tag with
+                  Cstr_constant n -> n
+                | _ -> assert false
+            in
+            let guardp = equals(Var guardvar, PInt true_tag) in
+            let env' = Lightenv.add guardvar f1 env in
+            let guard2 = And(guardp, guard) in
+            let (f2, cstrs2, fm2) = constrain e2 env' guard2 cstrs1 fm1 in
+            let guard3 = And(Not(guardp), guard) in
+            let (f3, cstrs3, fm3) = constrain e3 env' guard3 cstrs2 fm2 in
+              (f,
+               SubFrame(env', guard2, f2, f)
+               ::SubFrame(env', guard3, f3, f)
+               ::cstrs3,
+               fm3)
 (*	| Pexp_ident(Lident x) ->
             let f =
               let ty = ExpMap.find e shapemap in
@@ -65,17 +86,6 @@ let constrain_expression exp cstrs initframemap =
 		      (f'', SubType(env, guard, f2, f)::cs', fm')
 		| _ -> failwith "Subexpression frame has wrong shape - expected arrow"
 	    in List.fold_left constrain_application (constrain e1 env guard constrs framemap) exps
-	| Pexp_ifthenelse(e1, e2, Some e3) ->
-            let f = fresh_frame e in
-            let (f1, constrs''', fm''') = constrain e1 env guard constrs framemap in
-            let guardvar = fresh_bindvar() in
-            let env' = Env.add guardvar f1 env in
-            let guardp = equals(Var guardvar, PInt 1) in
-            let guard2 = And(guardp, guard) in
-            let (f2, constrs'', fm'') = constrain e2 env' guard2 constrs''' fm''' in
-            let guard3 = And(Not(guardp), guard) in
-            let (f3, constrs', fm') = constrain e3 env' guard3 constrs'' fm'' in
-              (f, SubType(env', guard2, f2, f)::SubType(env', guard3, f3, f)::constrs', fm')
 (*        | Exp.Match(e1, e2, (h, t), e3, _) ->
             begin match constrain e1 env guard constrs framemap with
                 (FList f1, constrs''', fm''') ->
@@ -105,16 +115,18 @@ let constrain_expression exp cstrs initframemap =
     in (f, cs, LocationMap.add e.exp_loc f fm)
   in constrain exp Lightenv.empty True cstrs initframemap
 
-let rec constrain_structure cstrs fmap = function
+(* pmr: note we're operating in the environment created by typing the
+   structure - it's entirely possible this has some bad corner cases *)
+let rec constrain_structure tenv cstrs fmap = function
   | [] -> (cstrs, fmap)
   | (Tstr_eval exp) :: srem ->
-      let (_, cstrs', fmap') = constrain_expression exp cstrs fmap in
-        constrain_structure cstrs' fmap' srem
+      let (_, cstrs', fmap') = constrain_expression tenv exp cstrs fmap in
+        constrain_structure tenv cstrs' fmap' srem
   | _ -> assert false
 
-let constrain_structure = constrain_structure [] LocationMap.empty
+let constrain_structure tenv = constrain_structure tenv [] LocationMap.empty
 
-let qualify_structure str =
-  let (cstrs, fmap) = constrain_structure str in
+let qualify_structure tenv str =
+  let (cstrs, fmap) = constrain_structure tenv str in
   let solution = solve_constraints Lightenv.empty cstrs in
     LocationMap.map (frame_apply_solution solution) fmap
