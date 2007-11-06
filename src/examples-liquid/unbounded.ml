533f18b09cad9ec33c51a8501f529f41cca4fc7d
Pat Rondon <pmr@meatwad.ucsd.edu>
2007-11-06 15:19:08 -0800

    Re-break unbounded.ml; it never worked, it just appeared to 'cause it didn't typecheck

--- src/examples-liquid/unbounded.ml
+++ src/examples-liquid/unbounded.ml
@@ -1,13 +1,13 @@
 let rec unb arr2 m n i j =
   if i = 0 then
     if j < n-1 then
-      if Bigarray.Array2.get arr2 0 j < 0 then unb 2 arr2 m n (i+1) j
-      else unb 1 arr2 m n 0 (j+1)
+      if Bigarray.Array2.get arr2 0 j < 0 then unb arr2 m n (i+1) j
+      else unb arr2 m n 0 (j+1)
     else false
   else
     if i < m then
-      if Bigarray.Array2.get arr2 i j < 0 then unb 2 arr2 m n (i+1) j
-      else unb 1 arr2 m n 0 (j+1)
+      if Bigarray.Array2.get arr2 i j < 0 then unb arr2 m n (i+1) j
+      else unb arr2 m n 0 (j+1)
     else true
 in
 
@@ -18,4 +18,4 @@ in
      (Random.int 10 + 1) (Random.int 20 + 1) in
  let m = Bigarray.Array2.dim1 arr in
  let n = Bigarray.Array2.dim2 arr in
-   unb 1 arr m n 0 1);;
+   unb arr m n 0 1);;
