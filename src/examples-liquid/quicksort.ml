c83f72806579716e79d3ec47e9f37c30d87f67ac
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2007-11-10 00:57:50 -0800

    hey guys, what's going on here?

--- src/examples-liquid/quicksort.ml
+++ src/examples-liquid/quicksort.ml
@@ -23,7 +23,7 @@ in
 (* There is a known performance bug in the code below.  If you find
    it, don't bother reporting it.  You're not supposed to use this
    module anyway. *)
-let sort cmp arr =
+(*let sort cmp arr =
   let rec qsort lo hi = 
     if hi - lo >= 6 then begin 
       let mid = (lo + hi)/2 in
@@ -32,16 +32,17 @@ let sort cmp arr =
          probability of picking a pathological pivot.  It also
          avoids extra comparisons on i and j in the two tight "while"
          loops below. *)
-      if cmp (Array.get arr mid) (Array.get arr lo) then swap arr mid lo;
+      if cmp (Array.get arr mid) (Array.get arr lo) then swap arr mid lo else ();
       if cmp (Array.get arr hi) (Array.get arr mid) then begin
         swap arr mid hi;
-        if cmp (Array.get arr mid) (Array.get arr lo) then swap arr mid lo
-      end;
+        if cmp (Array.get arr mid) (Array.get arr lo) then swap arr mid lo else ()
+      end else ();
       let pivot = Array.get arr mid in
-      let i = ref (lo + 1) and j = ref (hi - 1) in
+      let i = ref (lo + 1) in
+      let j = ref (hi - 1) in
       if not (cmp pivot (Array.get arr hi))
          || not (cmp (Array.get arr lo) pivot)
-      then assert false(*raise (Invalid_argument "Sort.array")*);
+      then assert false else ();
       let b1 () = !i < !j in
       let bod1 () = 
         let b2 () = cmp pivot (Array.get arr !i) in
@@ -50,7 +51,7 @@ let sort cmp arr =
         let b3 () = cmp (Array.get arr !j) pivot in
         let bod3 () = decr j in
           fwhile b3 bod3; 
-        if !i < !j then swap arr !i !j;
+        if !i < !j then swap arr !i !j else ();
         incr i; decr j
       in fwhile b1 bod1;
       (* Recursion on smaller half, tail-call on larger half *)
@@ -78,11 +79,11 @@ let sort cmp arr =
   in ffor 1 (Array.length arr - 1) forbod
 in
 let vec = [|3;5;6;2;4;6;3;345;7;4;2;2;57;3;6;8;3;5;7;4;6;3;6;124;64;34;123|]  in
-sort (<) vec; vec;;
+sort (<) vec; vec;;*)
 
 
-(*
-let array cmp arr =
+
+(*let sort cmp arr =
   let rec qsort lo hi =
     if hi - lo >= 6 then begin
       let mid = (lo + hi) lsr 1 in
@@ -91,19 +92,19 @@ let array cmp arr =
          probability of picking a pathological pivot.  It also
          avoids extra comparisons on i and j in the two tight "while"
          loops below. *)
-      if cmp (unsafe_get arr mid) (unsafe_get arr lo) then swap arr mid lo;
-      if cmp (unsafe_get arr hi) (unsafe_get arr mid) then begin
+      if cmp (Array.get arr mid) (Array.get arr lo) then swap arr mid lo;
+      if cmp (Array.get arr hi) (Array.get arr mid) then begin
         swap arr mid hi;
-        if cmp (unsafe_get arr mid) (unsafe_get arr lo) then swap arr mid lo
+        if cmp (Array.get arr mid) (Array.get arr lo) then swap arr mid lo
       end;
-      let pivot = unsafe_get arr mid in
+      let pivot = Array.get arr mid in
       let i = ref (lo + 1) and j = ref (hi - 1) in
-      if not (cmp pivot (unsafe_get arr hi))
-         || not (cmp (unsafe_get arr lo) pivot)
-      then raise (Invalid_argument "Sort.array");
+      if not (cmp pivot (Array.get arr hi))
+         || not (cmp (Array.get arr lo) pivot)
+      then assert false;
       while !i < !j do
-        while not (cmp pivot (unsafe_get arr !i)) do incr i done;
-        while not (cmp (unsafe_get arr !j) pivot) do decr j done;
+        while not (cmp pivot (Array.get arr !i)) do incr i done;
+        while not (cmp (Array.get arr !j) pivot) do decr j done;
         if !i < !j then swap arr !i !j;
         incr i; decr j
       done;
@@ -117,15 +118,18 @@ let array cmp arr =
   qsort 0 (Array.length arr - 1);
   (* Finish sorting by insertion sort *)
   for i = 1 to Array.length arr - 1 do
-    let val_i = (unsafe_get arr i) in
-    if not (cmp (unsafe_get arr (i - 1)) val_i) then begin
-      unsafe_set arr i (unsafe_get arr (i - 1));
+    let val_i = (Array.get arr i) in
+    if not (cmp (Array.get arr (i - 1)) val_i) then begin
+      Array.set arr i (Array.get arr (i - 1));
       let j = ref (i - 1) in
-      while !j >= 1 && not (cmp (unsafe_get arr (!j - 1)) val_i) do
-        unsafe_set arr !j (unsafe_get arr (!j - 1));
+      while !j >= 1 && not (cmp (Array.get arr (!j - 1)) val_i) do
+        Array.set arr !j (Array.get arr (!j - 1));
         decr j
       done;
-      unsafe_set arr !j val_i
+      Array.set arr !j val_i
     end
   done
+in 
+let vec = [|3;5;6;2;4;6;3;345;7;4;2;2;57;3;6;8;3;5;7;4;6;3;6;124;64;34;123|]  in
+  sort (<=) vec; vec;;
  *)
