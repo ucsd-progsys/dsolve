4691b6fbc6153f73749a68e8e0f87f5f89c89ba7
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2007-11-14 01:48:09 -0800

    split type checking and inferring at least some of the types we need in the rest of quicksort..

--- src/examples-liquid/split.ml
+++ src/examples-liquid/split.ml
@@ -7,7 +7,7 @@ let rec quickSort arr a n =
     in
     let rec bottom limit pa pb = 
       (*let _ = (fun x y -> (x, y)) pa pb in*)
-			let arg = {ft = pa; sd = pb} in
+			let arg = {p1 = pa; p2 = pb} in
       let ia = item a in
       (*let _ = (fun x -> x) pa in
       let _ = (fun x -> x) pb in*)
@@ -19,42 +19,39 @@ let rec quickSort arr a n =
 				if ipb < ia then bottom limit pa pb' else
 						(swap pa pb; bottom limit pa' pb')
 		in 
-    (*let rec top limit pc pd = 
-			let arg = {ft = pc; sd = pd} in
+    let rec top limit pc pd = 
+			let arg = {p1 = pc; p2 = pd} in
       let pc' = pc - 1 in
       let pd' = pd - 1 in
       if pc < limit then arg else
       if item pc < item a then arg else
 			if item a < item pc then top limit pc' pd else
 			(swap pc pd; top limit pc' pd') 
-    in*) 
+    in 
     let rec split pa pb pc pd =
 			let papb = bottom pc pa pb in
-      let pa = papb.ft in
-      let pb = papb.sd in
-      (*let pcpd = top pb pc pd in
-      let pc = pcpd.ft in
-      let pd = pcpd.sd in*)
+      let pa = papb.p1 in
+      let pb = papb.p2 in
+      let pcpd = top pb pc pd in
+      let pc = pcpd.p1 in
+      let pd = pcpd.p2 in
       let _ = (fun x -> x) pa in
       let _ = (fun x -> x) pb in
       let _ = (fun x -> x) pc in
       let _ = (fun x -> x) pd in
-      (*let pd = pcpd.sd in*)
-      if pb >= pc then {f = pa; s = pb; t = pc; g = pd}
+      if pb >= pc then {q1 = pa; q2 = pb; q3 = pc; q4 = pd}
       else 
-        let pc' = pc - 1 in
-        let pb' = pb + 1 in
       (swap pb pc; 
-								 split pa pb' pc' pd) 
+								 split pa (pb+1) (pc-1) pd) 
  	  in 
     let a' = a + 1 in
     let an = a + n in
     let an' = an - 1 in
     let spllit = split a' a' an' an' in
-    let pa = spllit.f in
-    let pb = spllit.s in
-    let pc = spllit.t in
-    let pd = spllit.g in
+    let pa = spllit.q1 in
+    let pb = spllit.q2 in
+    let pc = spllit.q3 in
+    let pd = spllit.q4 in
     let _ = (fun x -> x) pa in
     let _ = (fun x -> x) pb in
     let _ = (fun x -> x) pc in
@@ -65,4 +62,4 @@ let x = let x : garbage = 0 in x in
 let y = Random.int 20 + 1 in
 let vec = Array.make y x in
 let _ = (fun x -> x) vec in
-  quickSort vec 0 13;;
+  quickSort vec 0 (y-1);;
