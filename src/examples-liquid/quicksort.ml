911037709be823693968959917d5a095e26a2a1b
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2007-11-14 19:18:10 -0800

    sane quicksort is identical to Xi's quicksort except: pa, pd are maintained at the ends of the array vecswap at the right end of the array is commented out

--- src/examples-liquid/quicksort.ml
+++ src/examples-liquid/quicksort.ml
@@ -10,19 +10,6 @@ let ffor s d body =
   in dofor s
 in
 
-(*let fwhile b body =
-  let rec whiledo _n =
-    if b () then (body (); whiledo ()) else ()
-  in whiledo ()
-in
-
-let incr i =
-  i := !i + 1
-in
-let decr i =
-  i := !i - 1
-in*)
-
 let sort cmp arr =
   let rec qsort lo hi = 
     if hi - lo >= 6 then begin 
@@ -38,20 +25,19 @@ let sort cmp arr =
         if cmp (Array.get arr mid) (Array.get arr lo) then swap arr mid lo else ()
       end else ();
       let pivot = Array.get arr mid in
-      if not (cmp pivot (Array.get arr hi))
-         || not (cmp (Array.get arr lo) pivot)
-      then assert false else ();
       let rec bod1 i j = 
         if i < j then 
           let i =
             let rec bod2 i = 
-              if cmp pivot (Array.get arr i) 
-                then bod2 (i+1) else i
+              if i > j then i
+                 else if not(cmp pivot (Array.get arr i)) then i 
+                  else bod2 (i+1)  
             in bod2 i in
           let j =
             let rec bod3 j =
-            if cmp (Array.get arr j) pivot 
-                then bod3 (j-1) else j
+              if j < i then j
+                else if not(cmp (Array.get arr j) pivot) then j 
+                else bod3 (j-1) 
             in bod3 j in
               (if i < j then swap arr i j else ();
                bod1 (i+1) (j-1))
@@ -79,7 +65,9 @@ let sort cmp arr =
     end else ()
   in ffor 1 (Array.length arr - 1) forbod
 in
-let vec = [|19;18;17;16;15;14;13;12;11;10;9;8;7;6;5;4;3;2;1;0|]  in
+let p = Random.int 40 + 1 in
+let x = let x : garbage = 0 in x in
+let vec = Array.make p (x + x) in
 sort (<=) vec; vec;;
 
 
@@ -131,6 +119,5 @@ sort (<=) vec; vec;;
     end
   done
 in 
-let vec = [|3;5;6;2;4;6;3;345;7;4;2;2;57;3;6;8;3;5;7;4;6;3;6;124;64;34;123|]  in
   sort (<=) vec; vec;;
  *)
