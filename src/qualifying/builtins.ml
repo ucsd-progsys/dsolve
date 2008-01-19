d613bc1ab4941d78660142e1f8defc509d692fb2
Pat Rondon <pmr@famine>
2008-01-19 13:58:45 -0800

    Open modules in builtins; kill unused builtins

--- src/qualifying/builtins.ml
+++ src/qualifying/builtins.ml
@@ -1,8 +1,8 @@
 open Longident
 open Typedtree
+open Predicate
+open Frame
 open Asttypes
-
-module P = Predicate
 
 let rec mk_longid = function
   | [] -> assert false
@@ -15,52 +15,44 @@ let find_type_path id env =
   try fst (Env.lookup_type (mk_longid id) env)
   with Not_found -> Printf.printf "Couldn't load %s!\n" (String.concat " " id); assert false
 
-let qsize rel x y z = (Path.mk_ident ("SIZE_" ^ (P.pprint_rel rel)), y,
-                       P.Atom(P.Var z, rel, P.FunApp("Array.length", P.Var x)))
-
-let samesize y =
-  let x = Path.mk_ident "x" in
-    (Path.mk_ident ("SAME_SIZE_AS_" ^ (Path.name y)),
-     x,
-     P.Atom(P.FunApp("Array.length", P.Var x),
-                    P.Eq,
-                    P.FunApp("Array.length", P.Var y)))
+let qsize rel x y z = (Path.mk_ident ("SIZE_" ^ (pprint_rel rel)), y,
+                       Atom(Var z, rel, FunApp("Array.length", Var x)))
 
 let qdim rel dim x y z =
   let dimstr = string_of_int dim in
-    (Path.mk_ident ("DIM" ^ dimstr ^ (P.pprint_rel rel)), y,
-     P.Atom(P.Var z, rel,
-                    P.FunApp("Bigarray.Array2.dim" ^ dimstr, P.Var x)))
+    (Path.mk_ident ("DIM" ^ dimstr ^ (pprint_rel rel)), y,
+     Atom(Var z, rel, FunApp("Bigarray.Array2.dim" ^ dimstr, Var x)))
 
-let qint rel i y = (Path.mk_ident (Printf.sprintf "INT_%s%d" 
-		(P.pprint_rel rel) i), y, P.Atom(P.Var y, rel, P.PInt i))
+let qint rel i y =
+  (Path.mk_ident (Printf.sprintf "INT_%s%d" (pprint_rel rel) i), y, Atom(Var y, rel, PInt i))
 
-let qrel rel x y = (Path.mk_ident (Printf.sprintf "_%s%s%s_" 
-                                      (Path.name x) (P.pprint_rel rel) (Path.name y)), 
-                                      x, P.Atom(P.Var x, rel, P.Var y))
+let qrel rel x y =
+    (Path.mk_ident (Printf.sprintf "_%s%s%s_" (Path.name x) (pprint_rel rel) (Path.name y)), 
+     x,
+     Atom(Var x, rel, Var y))
 
 let qbool_rel qname rel (x, y, z) =
-  let truepred = P.Atom (P.Var x, rel, P.Var y) in
+  let truepred = Atom (Var x, rel, Var y) in
   (Path.mk_ident qname,
    z, 
-   P.Or (P.And (P.equals (P.Var z, P.PInt 1), truepred),
-         P.And (P.equals (P.Var z, P.PInt 0), P.Not truepred)))
+   Or (And (equals (Var z, PInt 1), truepred),
+       And (equals (Var z, PInt 0), Not truepred)))
 
-let mk_tyvar () = Frame.Fvar(Path.mk_ident "'a") 
+let mk_tyvar () = Fvar(Path.mk_ident "'a") 
 
-let mk_tyvarb () = Frame.Fvar(Path.mk_ident "'b")
+let mk_tyvarb () = Fvar(Path.mk_ident "'b")
 
-let mk_int qs = Frame.Fconstr(Predef.path_int, [], ([], Frame.Qconst qs))
+let mk_int qs = Fconstr(Predef.path_int, [], ([], Qconst qs))
 
-let mk_float = Frame.Fconstr(Predef.path_float, [], ([], Frame.Qconst []))
+let mk_float = Fconstr(Predef.path_float, [], ([], Qconst []))
 
-let mk_bool qs = Frame.Fconstr(Predef.path_bool, [], ([], Frame.Qconst qs))
+let mk_bool qs = Fconstr(Predef.path_bool, [], ([], Qconst qs))
 
-let mk_array f qs = Frame.Fconstr(Predef.path_array, [f], ([], Frame.Qconst qs))
+let mk_array f qs = Fconstr(Predef.path_array, [f], ([], Qconst qs))
 
-let mk_named path fs qs env = Frame.Fconstr(find_type_path path env, fs, ([], Frame.Qconst qs))
+let mk_named path fs qs env = Fconstr(find_type_path path env, fs, ([], Qconst qs))
 
-let mk_ref f env = Frame.Frecord (find_type_path ["ref"; "Pervasives"] env, [(f, "contents", Mutable)], ([], Frame.Qconst []))
+let mk_ref f env = Frecord (find_type_path ["ref"; "Pervasives"] env, [(f, "contents", Mutable)], ([], Qconst []))
 
 let mk_bigarray_kind a b qs env = mk_named ["kind"; "Bigarray"] [a; b] qs env
 
@@ -68,15 +60,15 @@ let mk_bigarray_layout a qs env = mk_named ["layout"; "Bigarray"] [a] qs env
 
 let mk_bigarray_type a b c qs env = mk_named ["t"; "Array2"; "Bigarray"] [a; b; c] qs env
 
-let mk_unit () = Frame.Fconstr(Predef.path_unit, [], ([], Frame.Qconst []))
+let mk_unit () = Fconstr(Predef.path_unit, [], ([], Qconst []))
 
 let mk_fun (path, f, f') =
   let x = match path with
   | Path.Pident id -> id
   | _ -> assert false
-  in Frame.Farrow (Some (Tpat_var x), f, f')
+  in Farrow (Some (Tpat_var x), f, f')
 
-let mk_int_equals x i = P.equals(P.Var x, P.PInt i)
+let mk_int_equals x i = equals(Var x, PInt i)
 
 let fun_frame path (x, y) qual =
   (path, mk_fun (x, mk_int [], mk_fun (y, mk_int [], mk_int [qual])))
@@ -94,7 +86,7 @@ let op_frame path qname op =
   let (x, y, z) = fresh_idents () in
   let qual = (Path.mk_ident qname,
               z,
-              P.equals (P.Var z, P.Binop (P.Var x, op, P.Var y)))
+              equals (Var z, Binop (Var x, op, Var y)))
   in fun_frame path (x, y) qual
 
 let uninterp_float_binop path =
@@ -115,19 +107,19 @@ let lsr_frame =
     (["lsr"; "Pervasives"],
      mk_fun(x, mk_int [],
             mk_fun (y, mk_int [],
-                    mk_int [qint P.Ge 0 z])))
+                    mk_int [qint Ge 0 z])))
 
 let land_frame =
   let (x, y, z) = fresh_idents () in
   let qual = (Path.mk_ident "land",
               z,
-              P.implies
-                (P.And (P.Atom (P.Var x, P.Ge, P.PInt 0),
-                                P.Atom (P.Var y, P.Ge, P.PInt 0)),
-                 P.big_and
-                   [P.Atom (P.PInt 0, P.Le, P.Var z);
-                    P.Atom (P.Var z, P.Le, P.Var x);
-                    P.Atom (P.Var z, P.Le, P.Var y);])) in
+              implies
+                (And (Atom (Var x, Ge, PInt 0),
+                                Atom (Var y, Ge, PInt 0)),
+                 big_and
+                   [Atom (PInt 0, Le, Var z);
+                    Atom (Var z, Le, Var x);
+                    Atom (Var z, Le, Var y);])) in
     (["land"; "Pervasives"],
      mk_fun(x, mk_int [],
             mk_fun (y, mk_int [],
@@ -137,12 +129,12 @@ let mod_frame =
   let (x, y, z) = fresh_idents () in
   let qual = (Path.mk_ident "mod",
               z,
-              P.implies
-                (P.And (P.Atom (P.Var x, P.Ge, P.PInt 0),
-                                P.Atom (P.Var y, P.Ge, P.PInt 0)),
-                 P.big_and
-                   [P.Atom (P.PInt 0, P.Le, P.Var z);
-                    P.Atom (P.Var z, P.Lt, P.Var y);])) in
+              implies
+                (And (Atom (Var x, Ge, PInt 0),
+                                Atom (Var y, Ge, PInt 0)),
+                 big_and
+                   [Atom (PInt 0, Le, Var z);
+                    Atom (Var z, Lt, Var y);])) in
     (["mod"; "Pervasives"],
      mk_fun(x, mk_int [],
             mk_fun (y, mk_int [],
@@ -152,33 +144,32 @@ let bool_conj_frame path qname =
   let (x, y, z) = fresh_idents () in
   let qual = (Path.mk_ident qname,
                 z,
-                P.Or( P.And(mk_int_equals z 1, 
-                              P.And(mk_int_equals x 1, mk_int_equals y 1)),
-                              P.And(mk_int_equals z 0,
-                              P.Or(mk_int_equals x 0, mk_int_equals y 0)) )) in
+                Or( And(mk_int_equals z 1, 
+                              And(mk_int_equals x 1, mk_int_equals y 1)),
+                              And(mk_int_equals z 0,
+                              Or(mk_int_equals x 0, mk_int_equals y 0)) )) in
     rel_fun_frame path (x, y) (mk_bool [], mk_bool []) qual
   
 let bool_disj_frame path qname =
   let (x, y, z) = fresh_idents () in
   let qual = (Path.mk_ident qname,
                 z,
-                P.Or( P.And(mk_int_equals z 1,
-                              P.Or(mk_int_equals x 1, mk_int_equals y 1)),
-                              P.And(mk_int_equals z 0,
-                              P.And(mk_int_equals x 0, mk_int_equals y 0)) )) in
+                Or( And(mk_int_equals z 1,
+                              Or(mk_int_equals x 1, mk_int_equals y 1)),
+                              And(mk_int_equals z 0,
+                              And(mk_int_equals x 0, mk_int_equals y 0)) )) in
     rel_fun_frame path (x, y) (mk_bool [], mk_bool []) qual
 
 let bool_not_frame =
   let (x, y) = fresh_2_idents () in
   let qual = (Path.mk_ident "NOT",
               y,
-              P.And( P.Or(mk_int_equals y 1,
+              And( Or(mk_int_equals y 1,
                                          mk_int_equals x 1),
-                             P.Or(mk_int_equals y 0,
+                             Or(mk_int_equals y 0,
                                          mk_int_equals x 0))) in
     (["not"; "Pervasives"], mk_fun (x, mk_bool [], mk_bool [qual]))
 
-    
 let ignore_frame =
   let x = Path.mk_ident "x" in
     (["ignore"; "Pervasives"], mk_fun (x, mk_tyvar (), mk_unit ())) 
@@ -192,44 +183,37 @@ let poly_rel_frame path qname rel =
   let tyvar = mk_tyvar () in
     poly_rel_fun_frame path (x, y) tyvar (qbool_rel qname rel (x, y, z))
 
-
 let array_length_frame = 
 	let (x, y) = (Path.mk_ident "x", Path.mk_ident "y") in (* make 2 idents fresh *)
 	let tyvar = mk_tyvar () in
-	(["length"; "Array"], mk_fun (x, (mk_array tyvar []), mk_int [qsize P.Eq x y y; qint P.Ge 0 y]))
+	(["length"; "Array"], mk_fun (x, (mk_array tyvar []), mk_int [qsize Eq x y y; qint Ge 0 y]))
 
 let array_set_frame =
   let (x, y, z) = fresh_idents () in
   let tyvar = mk_tyvar () in
   (["set"; "Array"], mk_fun(x, (mk_array tyvar []),
-                mk_fun(y, mk_int [qsize P.Lt x y y; qint P.Ge 0 y],
+                mk_fun(y, mk_int [qsize Lt x y y; qint Ge 0 y],
                 mk_fun(z, tyvar, mk_unit ()))))
 
 let array_get_frame =
 	let (x, y) = (Path.mk_ident "x", Path.mk_ident "y") in
 	let tyvar = mk_tyvar () in
 	(["get"; "Array"], mk_fun(x, (mk_array tyvar []), 
-				  mk_fun(y, mk_int [qsize P.Lt x y y; qint P.Ge 0 y], (tyvar))))
+				  mk_fun(y, mk_int [qsize Lt x y y; qint Ge 0 y], (tyvar))))
 
 let array_make_frame =
   let (x, y, z) = fresh_idents () in
   let tyvar = mk_tyvar () in
-  (["make"; "Array"], mk_fun(x, mk_int [qint P.Ge 0 x],
-                 mk_fun(y, tyvar, mk_array tyvar [qsize P.Eq z z x])))
-
-let array_copy_frame =
-  let (x, y) = fresh_2_idents () in
-  let tyvar = mk_tyvar () in
-  (["copy"; "Array"], mk_fun(x, mk_array tyvar [],
-                             mk_array tyvar [samesize x]))
+  (["make"; "Array"], mk_fun(x, mk_int [qint Ge 0 x],
+                 mk_fun(y, tyvar, mk_array tyvar [qsize Eq z z x])))
 
 let array_init_frame =
   let (x, y, z) = fresh_idents () in
   let i = Path.mk_ident "i" in
   let tyvar = mk_tyvar () in
-  let init = mk_fun (y, mk_int [qint P.Ge 0 y; qrel P.Lt y x], tyvar) in
-    (["init"; "Array"], mk_fun(x, mk_int [qint P.Ge 0 x],
-                               mk_fun(i, init, mk_array tyvar [qsize P.Eq z z x])))
+  let init = mk_fun (y, mk_int [qint Ge 0 y; qrel Lt y x], tyvar) in
+    (["init"; "Array"], mk_fun(x, mk_int [qint Ge 0 x],
+                               mk_fun(i, init, mk_array tyvar [qsize Eq z z x])))
 
 let bigarray_create_frame env =
   let (k, l) = fresh_2_idents () in
@@ -238,11 +222,11 @@ let bigarray_create_frame env =
   (["create"; "Array2"; "Bigarray"],
    mk_fun(k, mk_bigarray_kind a b [] env,
           (mk_fun(l, mk_bigarray_layout c [] env,
-          (mk_fun(dim1, mk_int [qint P.Gt 0 dim1],
-          (mk_fun(dim2, mk_int [qint P.Gt 0 dim2],
+          (mk_fun(dim1, mk_int [qint Gt 0 dim1],
+          (mk_fun(dim2, mk_int [qint Gt 0 dim2],
                   mk_bigarray_type a b c
-                    [qdim P.Eq 1 z z dim1;
-                     qdim P.Eq 2 z z dim2]
+                    [qdim Eq 1 z z dim1;
+                     qdim Eq 2 z z dim2]
                     env))))))))
 
 let bigarray_int_frame env =
@@ -263,11 +247,11 @@ let bigarray_get_frame env =
     (["get"; "Array2"; "Bigarray"],
      mk_fun(a, mk_bigarray_type ty (mk_tyvar ()) (mk_tyvar ()) [] env,
             mk_fun(i,
-                   mk_int [qint P.Ge 0 i;
-                           qdim P.Lt 1 a i i],
+                   mk_int [qint Ge 0 i;
+                           qdim Lt 1 a i i],
             mk_fun(j,
-                   mk_int [qint P.Ge 0 j;
-                           qdim P.Lt 2 a j j],
+                   mk_int [qint Ge 0 j;
+                           qdim Lt 2 a j j],
                    ty))))
 
 let bigarray_set_frame env =
@@ -277,11 +261,11 @@ let bigarray_set_frame env =
     (["set"; "Array2"; "Bigarray"],
      mk_fun(a, mk_bigarray_type ty (mk_tyvar ()) (mk_tyvar ()) [] env,
             mk_fun(i,
-                   mk_int [qint P.Ge 0 i;
-                           qdim P.Lt 1 a i i],
+                   mk_int [qint Ge 0 i;
+                           qdim Lt 1 a i i],
             mk_fun(j,
-                   mk_int [qint P.Ge 0 j;
-                           qdim P.Lt 2 a j j],
+                   mk_int [qint Ge 0 j;
+                           qdim Lt 2 a j j],
             mk_fun(v, ty, mk_unit ())))))
 
 let bigarray_dim_frame dim env =
@@ -289,7 +273,7 @@ let bigarray_dim_frame dim env =
   let (a, s) = fresh_2_idents () in
     (["dim" ^ string_of_int dim; "Array2"; "Bigarray"],
      mk_fun(a, mk_bigarray_type ty (mk_tyvar ()) (mk_tyvar ()) [] env,
-            mk_int [qdim P.Eq dim a s s; qint P.Gt 0 s]))
+            mk_int [qdim Eq dim a s s; qint Gt 0 s]))
 
 let rand_init_frame =
   let x = Path.mk_ident "x" in
@@ -301,8 +285,8 @@ let void_fun_frame name =
 
 let rand_int_frame =
   let (x, y) = fresh_2_idents () in
-  (["int"; "Random"], mk_fun(x, mk_int [qint P.Gt 0 x], 
-                                mk_int [qint P.Ge 0 y; qrel P.Lt y x]))
+  (["int"; "Random"], mk_fun(x, mk_int [qint Gt 0 x], 
+                                mk_int [qint Ge 0 y; qrel Lt y x]))
 
 let ref_ref_frame env =
   let (x, y) = fresh_2_idents () in
@@ -324,7 +308,7 @@ let tuple_fst_snd_frame name fst =
   let x = Path.mk_ident "x" in
   let tyvara = mk_tyvar () in
   let tyvarb = mk_tyvarb () in
-    (name, mk_fun(x, Frame.Ftuple [tyvara; tyvarb],
+    (name, mk_fun(x, Ftuple [tyvara; tyvarb],
                   if fst then tyvara else tyvarb))
 
 let ref_path env =
@@ -336,10 +320,10 @@ let bigarray_array2_path env =
 let max_int_frame = (["max_int"; "Pervasives"], mk_int [])
 
 let _frames = [
-  op_frame ["+"; "Pervasives"] "+" P.Plus;
-  op_frame ["-"; "Pervasives"] "-" P.Minus;
-  op_frame ["/"; "Pervasives"] "/" P.Div;
-  op_frame ["*"; "Pervasives"] "*" P.Times;
+  op_frame ["+"; "Pervasives"] "+" Plus;
+  op_frame ["-"; "Pervasives"] "-" Minus;
+  op_frame ["/"; "Pervasives"] "/" Div;
+  op_frame ["*"; "Pervasives"] "*" Times;
   uninterp_float_binop ["+."; "Pervasives"];
   uninterp_float_binop ["-."; "Pervasives"];
   uninterp_float_binop ["/."; "Pervasives"];
@@ -353,20 +337,19 @@ let _frames = [
   uninterp_int_binop ["lxor"; "Pervasives"];
   lsr_frame;
   uninterp_int_binop ["lsl"; "Pervasives"];
-  poly_rel_frame ["="; "Pervasives"] "=" P.Eq;
-  poly_rel_frame ["!="; "Pervasives"] "!=" P.Ne;
-  poly_rel_frame ["<>"; "Pervasives"] "<>" P.Ne;
-  poly_rel_frame ["<"; "Pervasives"] "<" P.Lt;
-  poly_rel_frame [">"; "Pervasives"] ">" P.Gt;
-  poly_rel_frame [">="; "Pervasives"] ">=" P.Ge;
-  poly_rel_frame ["<="; "Pervasives"] "<=" P.Le;
+  poly_rel_frame ["="; "Pervasives"] "=" Eq;
+  poly_rel_frame ["!="; "Pervasives"] "!=" Ne;
+  poly_rel_frame ["<>"; "Pervasives"] "<>" Ne;
+  poly_rel_frame ["<"; "Pervasives"] "<" Lt;
+  poly_rel_frame [">"; "Pervasives"] ">" Gt;
+  poly_rel_frame [">="; "Pervasives"] ">=" Ge;
+  poly_rel_frame ["<="; "Pervasives"] "<=" Le;
   tuple_fst_snd_frame ["fst"; "Pervasives"] true;
   tuple_fst_snd_frame ["snd"; "Pervasives"] false;
   bool_conj_frame ["&&"; "Pervasives"] "&&";
   bool_disj_frame ["||"; "Pervasives"] "||";
   bool_not_frame;
   array_length_frame;
-  array_copy_frame;
   array_get_frame;
   array_make_frame;
   array_init_frame;
@@ -403,7 +386,7 @@ let _type_paths = ref None
 let ext_find_type_path t =
   (fun (a, b) -> b) (List.find (fun (a, _) -> (a = t)) 
                       (match !_type_paths with None -> assert false
-                                               | Some b -> b))
+                                             | Some b -> b))
 
 let frames env =
   let _ = _type_paths := Some (List.map (fun x -> x env) _type_path_constrs) in
@@ -413,21 +396,21 @@ let frames env =
 
 let equality_refinement exp =
   let x = Path.mk_ident "V" in
-    let pred = P.equals (P.Var x, exp) in
-    P.pprint Format.str_formatter pred;
+    let pred = equals (Var x, exp) in
+    Predicate.pprint Format.str_formatter pred;
     let expstr = Format.flush_str_formatter () in
-      ([], Frame.Qconst [(Path.mk_ident expstr,
+      ([], Qconst [(Path.mk_ident expstr,
                           x,
                           pred)])
 
 let size_lit_refinement i =
-	let x = Path.mk_ident "x" in
-	  ([], Frame.Qconst [(Path.mk_ident "<size_lit_eq>",
-			      x,
-			      P.equals(P.FunApp("Array.length", P.Var x), P.PInt(i)))])
+  let x = Path.mk_ident "x" in
+    ([], Qconst [(Path.mk_ident "<size_lit_eq>",
+                  x,
+	              equals(FunApp("Array.length", Var x), PInt i))])
 
 let field_eq_qualifier name pexp =
   let x = Path.mk_ident "x" in
     (Path.mk_ident "<field_eq>",
      x,
-     P.equals (P.Field (name, P.Var x), pexp))
+     equals (Field (name, Var x), pexp))
