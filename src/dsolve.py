04615ca01a3e21475de615790fe7864b0c9b67a9
Pat Rondon <pmr@meatwad.ucsd.edu>
2008-02-29 10:34:33 -0800

    Fix dsolve.py, merge with fix-annot

--- src/dsolve.py
+++ src/dsolve.py
@@ -4,22 +4,57 @@ import sys
 import os
 import os.path
 
-ocaml="./liquid.run"
-qflags="-no-anormal -dqualifs -lqualifs"
-flags="-dframes "
-fname=sys.argv[len(sys.argv) - 1]
+gen="./liquid.opt -no-anormal -dqualifs -lqualifs"
+solve="./liquid.opt"
 
-os.system("rm -f %s.quals; rm -f %s.annot" % (fname, fname))
+flags = ["-dframes"]
+
+basename=sys.argv[len(sys.argv) - 1][:-3]
+fname = basename + ".ml"
+qname = basename + ".quals"
+aname = basename + ".annot"
+
+os.system("rm -f %s; rm -f %s" % (qname, aname))
 
 if (sys.argv[1] != "--bare"):
-  os.system("%s %s %s 1> /dev/null 2> %s.quals" % (ocaml, qflags, fname, fname))
+  os.system("%s %s 1> /dev/null 2> %s" % (gen, fname, qname))
 else:
-  os.system("touch %s.quals" % (fname))
+  os.system("touch %s" % (qname))
   sys.argv.pop()
 
-flags += " ".join(sys.argv[1:-1])
-os.system("%s %s %s" % (ocaml, flags, fname))
+os.system("%s %s %s" % (solve, " ".join(flags + sys.argv[1:-1]), fname))
 
-base = os.path.basename(fname)
-dir = os.path.dirname(fname)
-os.system("cat /tmp/liq.annot | ./fix-annot.py %s > %s/%s.annot" % (fname, dir, base))
+def fix_annotations():
+  qualfile = open(qname)
+  quallines = qualfile.readlines()
+  qualfile.close()
+
+  lineoffset = len(quallines)
+  charoffset = len("".join(quallines))
+
+  annotfile = open("/tmp/liq.annot")
+  fixedannots = []
+
+  for line in annotfile.readlines():
+      fields = line.split(" ")
+      if fields[0] == '"/tmp/liq.ml"':
+          for i in [0, 4]:
+              fields[i] = '"' + fname + '"'
+
+          for i in [1, 5]:
+              fields[i] = str(int(fields[i]) - lineoffset)
+
+          for j in [2, 3, 6]:
+              fields[j] = str(int(fields[j]) - charoffset)
+
+          fields[7] = str(int(fields[7]) - charoffset)
+
+      fixedannots.append(" ".join(fields))
+
+  annotfile.close()
+  return "\n".join(fixedannots)
+
+fixedannots = fix_annotations()
+annotfile = open(aname, "w")
+annotfile.write(fixedannots)
+annotfile.close()
