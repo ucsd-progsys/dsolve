dcdf3a3d5a1c55f0c08bfc2529df5185ce0c5c00
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2008-03-07 14:21:00 -0800

    changed examples-liquid to examples

--- src/examples/abs2.ml
+++ src/examples/abs2.ml
@@ -1,6 +1,8 @@
-qualifier NNEG(x) = 0 <= x;;
+qualif NNEG(x): 0 <= x
 
-let abs x = if x < 0 then (0 - x) else x in
-let s = abs 1 in
-let t = abs (-1) in
-  s;;
+let abs x = if x < 0 then (0 - x) else x
+
+let k =
+  let s = abs 1 in
+  let t = abs (-1) in
+    s
--- src/examples/empty.ml
+++ src/examples/empty.ml
@@ -1,5 +1,5 @@
 qual ISEMPTY(x): not(true);;
-
-? let n = [] in
+(* ? *)
+let n = [] in
   let l = 5::[] in
    l;;
