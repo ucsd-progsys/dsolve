cb852b5cc7ea3f7555caa78a5fed56cfbaaa2e98
Ranjit Jhala <rjhala@pho.(none)>
2008-03-07 19:00:12 -0800

    adding examples/tup1.ml

--- src/dsolve.py
+++ src/dsolve.py
@@ -64,8 +64,8 @@ else: flags += sys.argv[1:-1]
 os.system("rm -f %s.quals; rm -f %s.annot" % (bname, bname))	
 gen_quals(bname,bare)
 succ = solve_quals(tname,flags)
+fix_annots(tname,bname)
 if succ == 0:
-  fix_annots(tname,bname)
   sys.exit()
 else:
-  BogusErrorr
+  BogusError
