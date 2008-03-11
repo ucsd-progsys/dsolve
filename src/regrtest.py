b807855f259e9a935cf93c650cb29a97686a5f9e
Pat Rondon <pmr@meatwad.ucsd.edu>
2008-03-10 17:45:25 -0700

    Fix colors in regrtest.py (crosses fingers)

--- src/regrtest.py
+++ src/regrtest.py
@@ -13,9 +13,9 @@ def runtest(file, expected_status):
 
   ok = (status == expected_status)
   if ok:
-    print "\033[1;32mSUCCESS!\033[1;37m\n"
+    print "\033[1;32mSUCCESS!\033[1;0m\n"
   else:
-    print "\033[1;31mFAILURE :(\033[1;37m\n"
+    print "\033[1;31mFAILURE :(\033[1;0m\n"
   return (file, ok)
 
 def runtests(file, expected_status):
@@ -26,7 +26,7 @@ results   = [runtests(file, expected_status) for (file, expected_status) in test
 failed    = [result[0] for result in it.chain(*results) if result[1] == False]
 failcount = len(failed)
 if failcount == 0:
-  print "\n\033[1;32mPassed all tests! :D\033[1;37m"
+  print "\n\033[1;32mPassed all tests! :D\033[1;0m"
 else:
-  print "\n\033[1;31mFailed %d tests:\033[1;37m %s" % (failcount, ", ".join(failed))
+  print "\n\033[1;31mFailed %d tests:\033[1;0m %s" % (failcount, ", ".join(failed))
 sys.exit(failcount != 0)
