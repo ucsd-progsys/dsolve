920becc541ed2b987d7534e0d05767ad76656e87
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2008-05-14 20:35:02 -0700

    measure representation

--- liquid/measure.ml
+++ liquid/measure.ml
@@ -1,35 +1,41 @@
+open Types
+
 module P = Predicate
 module C = Constraint
 module F = Frame
 module Le = Lightenv
 
-let val_id = Path.mk_ident "_AA"
-let val_var = P.Var val_id
-
-type t = (constructor_tag * Path.t list * F.refinement) list Le.t
-
-type b_meas = { meas_cname: string; meas_def: P.t }
-
-let find_n tnm tag = Le.find 
-(*let find_c cstr = ()
-
-
-let add cstr = ()
-
-
-let addn = Le.addn
-let empty = Le.empty*)
+type mdef = (string * P.pexpr) 
+type m = (constructor_tag * Path.t option list * mdef list) list 
+type t = m Le.t
 
 let builtins = [
-  ("Some", "H", P.PInt(1)); 
-  ("None", "H", P.PInt(0));
+  ("Some", ([], [("H", P.PInt(1))])); 
+  ("None", ([], [("H", P.PInt(0))]));
 ]
 
-let proc_m mlenv =
-  let map_name c =  
+let empty = (Le.empty: m Le.t)
+
+let add (p, ((tag, _, _) as r)) env = 
+  let cs = try Le.find p env with
+             Not_found -> [] in
+  let t (a, b, c) = (a, (b, c)) in
+  let cs' = List.map t cs in
+  let _ = try ignore(List.assoc tag cs'); failwith "constructor redef in measure" with 
+            Not_found -> () in
+  Lightenv.add p (r :: cs) env
+
+let transl_desc mlenv (c, (ps, rs)) =
+  let c =  
     (Env.lookup_constructor (Longident.parse c) mlenv) in
-  let p (c, m, e) = { meas_cname = c; meas_def = P.Atom(P.FunApp(m, [val_var]), P.Eq, e) } in
-   List.map p builtins 
+  let tag = c.cstr_tag in
+  let fr = F.fresh_without_vars mlenv c.cstr_res in
+  let p = function
+    F.Fconstr(p, _, _) -> p
+    | _ -> failwith "constructor result is not a constructed type?" in
+  (p fr, (tag, ps, rs)) 
 
 let bms = ref empty
-let make_bms = bms := addn proc_m 
+let make_bms env = 
+  let f g e h = add (g h) e in
+  bms := List.fold_left (f (transl_desc env)) empty builtins 
