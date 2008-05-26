85f68a7f0d8e87ad06234cbbcdc4375e30bb47c1
Pat Rondon <pmr@meatwad.ucsd.edu>
2008-05-25 17:17:25 -0700

    Add tree generation examples

---
+++ tests/mktree.ml
@@ -0,0 +1,35 @@
+type tree = Empty | Node of int * tree * tree
+
+let rec mktree l h =
+  if h < l then
+    Empty
+  else
+    let r = (l + h) / 2 in
+      Node (r, mktree l (r - 1), mktree (r + 1) h)
+
+let rec checkleft x t =
+  match t with
+    | Empty -> ()
+    | Node (v, l, r) ->
+        assert (v <= x);
+        checkleft v l;
+        checkright v r
+
+and checkright x t =
+  match t with
+    | Empty -> ()
+    | Node (v, l, r) ->
+        assert (v >= x);
+        checkleft v l;
+        checkright v r
+
+let checktree t =
+  match t with
+    | Empty -> ()
+    | Node (v, l, r) ->
+        checkleft v l;
+        checkright v r
+
+let t = mktree 0 (Random.int 100)
+
+let _ = checktree t
