4c0426bd9e1088f8166899a916775a961088fc08
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2008-06-02 14:45:39 -0700

    workaround for embedding isuue and probably bug in propagating measures

--- tests/recmeasures.ml
+++ tests/recmeasures.ml
@@ -1,13 +1,17 @@
 let rec len (x: int list) = 
   match x with
-      [] -> let x = 0 in x + 0
+      [] -> 0 
     | s :: ss -> 1 + (len ss)
 
 let y = []
 let zz = (fun x -> x) (len y)
 let z = 1 :: y
+let zzz = 1 :: z
 
 let x = (fun x -> x) (len z)
  
-(*let blah = assert (len y = 0)*)
+let blah = assert (len z = 1)
+let blah2 = assert (len y = 0)
+let blah3 = assert (len zzz = 2)
+(*let blah4 = assert (len zzz = 1a)*)
 
