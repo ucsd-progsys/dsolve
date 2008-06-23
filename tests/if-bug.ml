16ef9c1ac6326d09b07f69e9bf78351e48f81e34
Pat Rondon <pmr@famine>
2008-06-23 09:16:23 -0700

    Shorten if bug test

--- tests/if-bug.ml
+++ tests/if-bug.ml
@@ -1,16 +1,13 @@
 let show = fun x -> x
 
-let rec spin x = spin x
-
 let f0 () = 0
 
 let make_int n =
   if n = 0 then show (* comment out "show" and assert fails! *)  (f0 ()) else
-      spin ()
+      n
 
 let _ = show make_int
 
 let check n = 
-  let _ = assert (n = make_int n) in
-  ()
+  assert (n = make_int n)
 
