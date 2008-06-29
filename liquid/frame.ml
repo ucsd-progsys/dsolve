7a01265d891b2141f4222e6653f54b76bf4baa69
Pat Rondon <pmr@famine>
2008-06-29 15:23:09 -0700

    Beautification

--- liquid/frame.ml
+++ liquid/frame.ml
@@ -609,15 +609,15 @@ let fresh_sum fresh env p t tyl varis =
   let param_vars = List.combine tyl varis in
   let (_, cds)   = List.split (Env.constructors_of_type p (Env.find_type p env)) in
   let fresh_cstr cstr =
-    let _ = Ctype.begin_def () in
+    let _           = Ctype.begin_def () in
     let (args, res) = Ctype.instance_constructor cstr in
-    let _ = Ctype.end_def () in
-    let _ = List.iter Ctype.generalize args; Ctype.generalize res in
-    let _ = close_constructor res args; Ctype.unify env res t in
+    let _           = Ctype.end_def () in
+    let _           = List.iter Ctype.generalize args; Ctype.generalize res in
+    let _           = close_constructor res args; Ctype.unify env res t in
     let (res, args) = (canonicalize res, List.map canonicalize args) in
-    let ids  = Misc.mapi (fun _ i -> C.tuple_elem_id i) args in
-    let fs   = List.map fresh args in
-    let vs   = List.map (fun t -> try List.assoc t param_vars with Not_found -> Covariant) args in
+    let ids         = Misc.mapi (fun _ i -> C.tuple_elem_id i) args in
+    let fs          = List.map fresh args in
+    let vs          = List.map (fun t -> try List.assoc t param_vars with Not_found -> Covariant) args in
       (cstr.cstr_tag, C.combine3 ids fs vs)
   in Fsum (p, None, List.map fresh_cstr cds, empty_refinement)
 
