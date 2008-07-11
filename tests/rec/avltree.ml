e1a28d83da87669846146c5bbe9bc92cfb223122
Ranjit Jhala <rjhala@pho.(none)>
2008-07-11 16:40:47 -0700

    checking in avltree stuff

--- tests/rec/avltree.ml
+++ tests/rec/avltree.ml
@@ -1,19 +1,19 @@
 type 'a avl = 
   | E 
-  | Bl of 'a * 'a avl * 'a avl * int (* lh bigger *)
-  | Br of 'a * 'a avl * 'a avl * int (* rh bigger *)
+  | Bl of 'a * 'a avl * 'a avl * int (* lh bigger: height v <= height e1 <= 1 + height v  *)
+  | Br of 'a * 'a avl * 'a avl * int (* rh bigger: height e1 <= height v <= 1 + height e1  *)
 
-let rec len = function
+let rec len t =
+  match t with 
   | E -> 0
   | Bl (_, l, r, h) -> 1 + len l + len r
   | Br (_, l, r, h) -> 1 + len l + len r
 
-let rec height = function
+let rec height t =
+  match t with
   | E -> 0
   | Bl (_, _, _, h) -> h
   | Br (_, _, _, h) -> h
-
-
 
 let left_rotate e l r =
   match l with
@@ -50,14 +50,13 @@ let right_rotate e l r =
         | Br (rle, rll, rlr, _ ) -> 
             Bl (rle, Bl (e, l, rll, rh-1), Br (re, rlr, rr, rrh+1), rh)
       else Bl (re, Br (e, l, rl, rlh+1), rr, rlh+2)
-  
-exception Item_is_found
 
 let rec insert e t = 
   match t with 
   | E -> Bl (e, E, E, 1)
   | Bl (e', l, r, h) ->
       if e < e' then
+        let foo = len t in
         let l' = insert e l in
         let lh' = height l' in
         let rh = height r in
@@ -85,4 +84,9 @@ let rec insert e t =
         if rh' <= lh then Bl (e', l, r', lh+1)
         else if rh' <= lh+1 then Br (e', l, r', rh'+1)
         else right_rotate e' l r'
-      else t
+      else t 
+
+let rec foo n = 
+  if n <= 0 then E else insert n (foo (n-1))
+
+let _ = foo 1000
