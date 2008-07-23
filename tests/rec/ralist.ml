95800ea77a5a278a1a57ac280ac2c30f9d7d2450
Pat Rondon <pmr@meatwad.ucsd.edu>
2008-07-22 18:07:57 -0700

    modified ralist to find bug

--- tests/rec/ralist.ml
+++ tests/rec/ralist.ml
@@ -13,6 +13,13 @@ let rec sz l =
     | Even (l1, l2) -> (sz l1) + (sz l2)
     | Odd (_, l1, l2) -> (1 + (sz l1) + (sz l2)) 
 
+let nil l =
+  match l with
+    | Nil           -> 1
+    | One (_)       -> 0
+    | Even (_, _)   -> 0
+    | Odd (_, _, _) -> 0
+
 let rec cons x xs =
   match xs with
     | Nil -> (One x)
@@ -25,15 +32,16 @@ let rec makelist n =
     let l = (makelist (n-1)) in
       cons n l 
 
+let show x = x
+
 let rec uncons xs =
+  let _ = show xs in
   match xs with
     | Nil -> assert (1 = 0); assert false
     | One x -> (x, Nil)
     | Even(l1, l2) ->
-        let _ = assert (sz l1 = sz l2) in
         let (x, l1) = uncons l1 in
         let (y, l2) = uncons l2 in
-        let _ = assert (sz l1 = sz l2) in
           begin
             match l1 with
                 Nil -> (x, One y)
@@ -42,7 +50,7 @@ let rec uncons xs =
               | Odd _ -> (x, Odd(y, l1, l2))
           end
     | Odd(x, l1, l2) -> (x, Even(l1, l2))
-
+(*
 let head_safe l =
   let (x, _) = uncons l in x
 
@@ -73,28 +81,37 @@ let rec lookup l i =
         if i = 0 then x
         else if 2 * (i / 2) = i then lookup l2 ((i - 1) / 2) 
         else lookup l1 ((i - 1) / 2)
+*)
 
 let rec print_rlist l =
   match l with
       Nil -> ()
-    | One _ -> let (x, _) = uncons l in print_int x; print_newline ()
-    | Even _ -> let (x, l) = uncons l in print_int x; print_rlist l
-    | Odd _ -> let (x, l) = uncons l in print_int x; print_rlist l
+    | One (x) -> 
+        let _ = show l in
+        let (x, _) = uncons l in print_int x; print_newline ()
+    | Even (a, b) -> 
+        let _ = show l in
+        let (x, l) = uncons l in print_int x; print_rlist l
+    | Odd (a, b, c) ->
+        let _ = show l in
+        let (x, l) = uncons (show l) in print_int x; print_rlist (show l)
 
 let check x n n' i =
   if n > 0 then
     let xs = makelist n in
-    let _ = sz xs in
-    let _ = assert (n = sz xs) in
-    let _ =
+    let _ = show xs in
+    let _ = print_rlist xs in
+
+(*    let _ =
       let ys = uncons xs in
         assert(sz (snd ys) = sz xs - 1) in
     let _ = head xs in
     let _ = tail xs in
-    let _ = print_rlist xs in
+
     let _ = 
       if (i < n) && (i >= 0) then 
         let _ = lookup xs i in ()
-      else ()
-    in ()
+      else () *)
+
+      ()
   else ()
