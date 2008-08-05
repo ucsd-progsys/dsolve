6c6edda4ddc5e4ab01469a4ac75d03bd6a9710dd
Pat Rondon <pmr@meatwad.ucsd.edu>
2008-08-05 15:48:20 -0700

    Add Filliatre's heap library to the tests; just checks heap property so far

---
+++ tests/rec/heap.ml
@@ -0,0 +1,102 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  Copyright (C) Jean-Christophe Filliatre                               *)
+(*                                                                        *)
+(*  This software is free software; you can redistribute it and/or        *)
+(*  modify it under the terms of the GNU Library General Public           *)
+(*  License version 2.1, with the special exception on linking            *)
+(*  described in file LICENSE.                                            *)
+(*                                                                        *)
+(*  This software is distributed in the hope that it will be useful,      *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *)
+(*                                                                        *)
+(**************************************************************************)
+
+(*s Heaps *)
+
+(* Heaps are encoded as complete binary trees, i.e., binary trees
+   which are full expect, may be, on the bottom level.
+   These trees also enjoy the heap property, namely the value of any node
+   is greater or equal than those of its left and right subtrees.
+
+   The representation invariant is the following: the number of nodes in
+   the left subtree is equal to the number of nodes in the right
+   subtree, or exceeds it by exactly once. In the first case, we use
+   the constructor [Same] and in the second the constructor [Diff].
+   Then it can be proved that [2^(h-1) <= n <= 2^h] when [n] is the
+   number of elements and [h] the height of the tree. *)
+
+type 'a t =
+  | Empty
+  | Same of 'a * 'a t * 'a t (* same number of elements on both sides *)
+  | Diff of 'a * 'a t * 'a t (* left has [n+1] nodes and right has [n] *)
+
+let empty = Empty
+
+let rec add x = function
+  | Empty ->
+      Same (x, Empty, Empty)
+        (* insertion to the left *)
+  | Same (y, l, r) ->
+      if x > y then Diff (x, add y l, r) else Diff (y, add x l, r)
+        (* insertion to the right *)
+  | Diff (y, l, r) ->
+      if x > y then Same (x, l, add y r) else Same (y, l, add x r)
+
+let maximum = function
+  | Empty -> assert false (* raise EmptyHeap *)
+  | Same (x, l, r) -> (x, Same (x, l, r)) | Diff (x, l, r) -> (x, Diff (x, l, r))
+
+(* extracts one element on the bottom level of the tree, while
+   maintaining the representation invariant *)
+let rec extract_last = function
+  | Empty -> assert false (* raise EmptyHeap *)
+  | Same (x, Empty, Empty) -> x, Empty
+  | Same (x, l, r) -> let y,r' = extract_last r in y, Diff (x, l, r')
+  | Diff (x, l, r) -> let y,l' = extract_last l in y, Same (x, l', r)
+
+(* removes the topmost element of the tree and inserts a new element [x] *)
+let rec descent x = function
+  | Empty ->
+      assert false
+  | Same (_, Empty, Empty) ->
+      Same (x, Empty, Empty)
+  | Diff (_, Same (z, ll, lr), Empty) ->
+      if x > z then Diff (x, Same (z, ll, lr), Empty)
+      else Diff (z, Same (x, Empty, Empty), Empty)
+  | Same (_, l, r) ->
+      let (ml, l) = maximum l in
+      let (mr, r) = maximum r in
+        if x > ml && x > mr then
+	  Same (x, l, r)
+	else
+	  if ml > mr then
+	    Same (ml, descent x l, r)
+	  else
+	    Same (mr, l, descent x r)
+  | Diff (_, l, r) ->
+      let (ml, l) = maximum l in
+      let (mr, r) = maximum r in
+	if x > ml && x > mr then
+	  Diff (x, l, r)
+	else
+	  if ml > mr then
+	    Diff (ml, descent x l, r)
+	  else
+	    Diff (mr, l, descent x r)
+
+let remove = function
+  | Empty -> assert false (* raise EmptyHeap *)
+  | Same (x, Empty, Empty) -> Empty
+  | h -> let y,h' = extract_last h in descent y h'
+
+let rec iter f = function
+  | Empty -> ()
+  | Same (x, l, r) -> iter f l; f x; iter f r
+  | Diff (x, l, r) -> iter f l; f x; iter f r
+
+let rec fold f h x0 = match h with
+  | Empty -> x0
+  | Same (x, l, r) -> fold f l (fold f r (f x x0))
+  | Diff (x, l, r) -> fold f l (fold f r (f x x0))
