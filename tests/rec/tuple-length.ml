80f21083dc9d8750088f87d910280fdce8db119c
Pat Rondon <pmr@meatwad.ucsd.edu>
2008-08-07 12:59:24 -0700

    Generalize match guard building to give measures for deep matches, add regrtests

---
+++ tests/rec/tuple-length.ml
@@ -0,0 +1,9 @@
+let rec length x = match x with
+  | []      -> 0
+  | _ :: xs -> 1 + length xs
+
+let rec testlength xs = match (xs, 3) with
+  | (x :: y :: zs, _) ->
+      let s = length zs in
+      assert (length xs >= 2)
+  | _ -> ()
