929c1ed031c502f547b01689c66355e4e1cd5334
Ranjit Jhala <rjhala@pho.(none)>
2008-09-27 15:33:30 -0700

    adding in mergesort to isortset

--- tests/isortset.ml
+++ tests/isortset.ml
@@ -10,6 +10,8 @@ let rec append xs ys =
   match xs with
   | []          -> ys
   | x::xs'      -> x::(append xs' ys)
+  
+(************************* InsertSort *************************)
 
 let rec ins x ys = 
   match ys with 
@@ -21,18 +23,52 @@ let rec insert_sort xs =
   | []     -> []
   | x::xs' -> ins x (insert_sort xs')
 
+(************************* Quicksort **************************)
+
 let rec quick_sort xs = 
   match xs with
   | []          -> []
   | x::xs'      -> let (gs,ls) = partition ((<) x) xs' in
                    append (quick_sort ls) (x::(quick_sort gs))
 
+(************************* Mergesort **************************)
+
+let rec halve xs =
+  match xs with
+  | []          -> ([], [])
+  | x::xs'      -> let (ys, zs) = halve xs' in 
+                   (x::zs, ys)
+
+let rec merge xs ys =
+  match xs with
+  | []          -> ys
+  | x::xs'      ->
+      begin
+        match ys with
+        | []     -> xs
+        | y::ys' -> 
+            if x < y 
+            then x::(merge xs' (y::ys')) 
+            else y::(merge (x::xs') ys')
+      end
+
+let rec merge_sort xs =
+  match xs with
+  | []          -> []
+  | x::xs'      -> 
+      (match xs' with [] -> [x] | _ -> 
+        let (ys, zs) = halve xs in
+        let ys'      = merge_sort ys in
+        let zs'      = merge_sort zs in
+        merge ys' zs')
+
+(*************************** Tests ****************************)
+
 let test xs =
   let _ = let ys = insert_sort xs in 
           assert (Myset.eq (ss xs) (ss ys)) in
   let _ = let ys = quick_sort xs in
           assert (Myset.eq (ss xs) (ss ys)) in
+  let _ = let ys = merge_sort xs in
+          assert (Myset.eq (ss xs) (ss ys)) in
   ()
-
-
-
