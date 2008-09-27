9c97d3260ba02f5f3122c562173a5393ebdfbfe5
ming <mwookawa@wideload.(none)>
2008-09-26 18:13:10 -0700

    set examples

--- tests/mysetlist.ml
+++ tests/mysetlist.ml
@@ -3,7 +3,7 @@ let y = Myset.mem
 let rec append xs ys =
   match xs with
   | [] -> let _ = assert 
-    (Myset.eq (Myset.of_list ys) (Myset.cup (Myset.of_list ys) (Myset.of_list xs))) in 
+    ((Myset.of_list ys) = (Myset.cup (Myset.of_list ys) (Myset.of_list xs))) in 
     let _ = assert (Myset.of_list xs = Myset.empty) in
     ys
   | x::xs' -> x::(append xs' ys)
--- tests/mysetpos.ml
+++ tests/mysetpos.ml
@@ -1,44 +1,45 @@
 let x = Myset.empty
-let y = Myset.eq x x
+let eq = Myset.eq
+let y = eq x x
 
 let _ = assert (y)
 
 let f a b =
-   assert (Myset.eq (Myset.cup a b) (Myset.cup b a))
+   assert (eq (Myset.cup a b) (Myset.cup b a))
 
 let f a b =
-   assert (Myset.eq (Myset.cup a b) (Myset.cup b a))
+   assert (eq (Myset.cup a b) (Myset.cup b a))
 
 let f a = 
-   assert (Myset.eq (Myset.cup a x) a)
+   assert (eq (Myset.cup a x) a)
 
 let f a = 
-   assert (Myset.eq (Myset.cap a x) x)
+   assert (eq (Myset.cap a x) x)
 
 let f a = 
-   assert (Myset.eq (Myset.cup a a) a)
+   assert (eq (Myset.cup a a) a)
 
 let f a = 
-   assert (Myset.eq (Myset.cap a a) a)
+   assert (eq (Myset.cap a a) a)
 
 let f a b = 
-   if Myset.eq a b then
-    assert (Myset.eq (Myset.cup a b) a)
+   if eq a b then
+    assert (eq (Myset.cup a b) a)
    else assert false
 
 let f a b =
-   if Myset.eq a b then
-    assert (Myset.eq (Myset.cap a b) a)
+   if eq a b then
+    assert (eq (Myset.cap a b) a)
    else assert false
 
 let f z a =
    if Myset.mem z a then
-    assert (Myset.eq (Myset.cup a (Myset.sng z)) a)
+    assert (eq (Myset.cup a (Myset.sng z)) a)
    else assert false
 
 let f z a b =
-   if Myset.eq a b then
-    assert (Myset.eq (Myset.cup a (Myset.sng z)) (Myset.cup b (Myset.sng z)))
+   if eq a b then
+    assert (eq (Myset.cup a (Myset.sng z)) (Myset.cup b (Myset.sng z)))
    else assert false
 
 
