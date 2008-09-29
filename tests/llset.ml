21273f8cf1b9bf2461ecfe790474d6a8ef17b342
Ranjit Jhala <rjhala@pho.(none)>
2008-09-28 17:00:48 -0700

    adding stuff to llset, membership/deletion etc.

--- tests/llset.ml
+++ tests/llset.ml
@@ -1,3 +1,6 @@
+let ss = Myset.eq
+
+(*
 type 'a list2 = Nil2 | Cons2 of 'a list * 'a list2
 
 let rec of_list2 xss =
@@ -14,7 +17,7 @@ let rec flatten xss =
   match xss with
   | Nil2                -> []
   | Cons2(xs,xss')      -> append xs (flatten xss')
-
+*)
 (*
 let rec of_list2 xss =
   match xss with
@@ -26,3 +29,50 @@ let rec flatten xss =
   | []          -> []
   | xs::xss'    -> xs @ (flatten xss')
 *)
+(*
+let rec reverse xs = 
+  match xs with
+  | []          -> []
+  | x::xs'      -> append (reverse xs') [x]
+
+let reverse2 xs = 
+  let rec f ys zs = 
+    match ys with
+    | []        -> zs
+    | y::ys'    -> f ys' (y::zs) in
+  f xs []
+
+let reverse3 xs = 
+  let rec f (ys,zs) = 
+    match ys with
+    | []        -> zs
+    | y::ys'    -> f (ys', (y::zs)) in
+  f (xs,[])
+
+*)
+
+let rec mem x ys = 
+  match ys with
+  | []          -> false
+  | y::ys'      -> if x = y then true else mem x ys'
+
+let rec delete x ys = 
+  match ys with
+  | []          -> []
+  | y::ys'      -> if x = y then delete x ys' else x::(delete x ys') 
+
+(*
+let compact xs = 
+  let rec f ys zs =
+    match zs with
+    | []        -> ys
+    | z::zs'    -> if mem z ys then f ys zs' else f (z::ys) zs' in
+  reverse (f [] xs)
+
+let rec sorted_compact xs =
+  match xs with
+  | []          -> []
+  | [x]         -> [x]
+  | x::x'::xs'  -> if x = x' then compact (x'::xs') else x::(compact (x'::xs'))
+*)
+
--- tests/myset.mli
+++ tests/myset.mli
@@ -1,9 +1,10 @@
 type set
 
-val of_list: 'a list -> set
-val cap:  set ->  set ->  set
-val cup:  set ->  set ->  set
-val mem:  'a ->  set -> bool
-val sng: 'a ->  set
-val empty:  set
+val empty: set
+val mem: 'a -> set -> bool
 val eq:  set ->  set -> bool
+val of_list: 'a list -> set
+val cap: set -> set -> set
+val cup: set -> set -> set
+val sng: 'a -> set
+val mns: set -> set -> set 
