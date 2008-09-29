2dccff3180dbfe8319b67ac59367d13e5bf361ee
Pat Rondon <pmr@meatwad.ucsd.edu>
2008-09-29 07:47:46 -0700

    Remove useless provers and libs

--- external/qp/qProver.mli
+++
@@ -1,8 +0,0 @@
-
-(** check_imp p q = valid(p => q) *) 
-val check_imp: QpDag.predicate -> (QpDag.predicate -> bool)
-
-(** check_imps p [q1;...;qn] = [b1;...;bn] where bi <=> valid(p => qi) *) 
-val check_imps: QpDag.predicate -> QpDag.predicate list -> bool list
-
-val print_stats: unit -> unit
