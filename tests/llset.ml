dad0f6289986b593bcfb785661ddf334025b93c5
Ranjit Jhala <rjhala@pho.(none)>
2008-09-28 17:44:34 -0700

    adding set minus operators and tests (for deleting stuff)

--- tests/llset.ml
+++ tests/llset.ml
@@ -1,6 +1,10 @@
 let ss = Myset.eq
 
-(*
+let rec append xs ys =
+  match xs with
+  | []          -> ys
+  | x::xs'      -> x::(append xs' ys)
+
 type 'a list2 = Nil2 | Cons2 of 'a list * 'a list2
 
 let rec of_list2 xss =
@@ -8,16 +12,11 @@ let rec of_list2 xss =
   | Nil2                -> Myset.empty
   | Cons2 (xs,xss')     -> Myset.cup (Myset.of_list xs) (of_list2 xss')
 
-let rec append xs ys =
-  match xs with
-  | []          -> ys
-  | x::xs'      -> x::(append xs' ys)
-
 let rec flatten xss =
   match xss with
   | Nil2                -> []
   | Cons2(xs,xss')      -> append xs (flatten xss')
-*)
+
 (*
 let rec of_list2 xss =
   match xss with
@@ -29,7 +28,7 @@ let rec flatten xss =
   | []          -> []
   | xs::xss'    -> xs @ (flatten xss')
 *)
-(*
+
 let rec reverse xs = 
   match xs with
   | []          -> []
@@ -42,8 +41,10 @@ let reverse2 xs =
     | y::ys'    -> f ys' (y::zs) in
   f xs []
 
+(* STRANGE ERROR // assert fails in liquid/pattern.ml
+
 let reverse3 xs = 
-  let rec f (ys,zs) = 
+  let rec f (ys, zs) = 
     match ys with
     | []        -> zs
     | y::ys'    -> f (ys', (y::zs)) in
@@ -59,14 +60,15 @@ let rec mem x ys =
 let rec delete x ys = 
   match ys with
   | []          -> []
-  | y::ys'      -> if x = y then delete x ys' else x::(delete x ys') 
+(*| y::ys'      -> if x = y then delete x ys' else x::(delete x ys')  (* BUG *) *)
+  | y::ys'      -> if x = y then delete x ys' else y::(delete x ys')  (* OK  *)
 
-(*
 let compact xs = 
   let rec f ys zs =
     match zs with
     | []        -> ys
-    | z::zs'    -> if mem z ys then f ys zs' else f (z::ys) zs' in
+(*  | z::zs'    -> if mem z xs then f ys zs' else f (z::ys) zs' in (* BUG *) *)
+    | z::zs'    -> if mem z ys then f ys zs' else f (z::ys) zs' in (* OK  *) 
   reverse (f [] xs)
 
 let rec sorted_compact xs =
@@ -74,5 +76,8 @@ let rec sorted_compact xs =
   | []          -> []
   | [x]         -> [x]
   | x::x'::xs'  -> if x = x' then compact (x'::xs') else x::(compact (x'::xs'))
-*)
 
+let rec diff xs ys = 
+  match xs with
+  | []          -> []
+  | x::xs'      -> if mem x ys then diff xs' ys else x::(diff xs' ys)
