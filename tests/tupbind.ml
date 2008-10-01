fc03a19bcfa0676511bf2bf2b06587103b62231e
Ranjit Jhala <rjhala@pho.(none)>
2008-10-01 14:18:47 -0700

    simplified tupbind bug

--- tests/tupbind.ml
+++ tests/tupbind.ml
@@ -1,4 +1,5 @@
 let show x = x
+(*
 let foo k = (k-2, k-1)
 let _     =
   let (p,q) = foo 10 in
@@ -26,9 +27,28 @@ let test3 x =
     let a = read_int () in
     let i = read_int () in
     if a < i then show (a, [i]) else assert false in
-  let (b,js) = show w in
+  let (b,js) = w in
   List.iter (fun j -> assert (b < j)) js
 
+*)
+
+type foo = C of int
+
+let test4 x =
+  let w = 
+    let a = read_int () in
+    let i = read_int () in
+    if a < i then (a, C i) else assert false in
+  let (b,js) = w in
+  match js with C j -> assert (b < j)
+
+let test5 x =
+  let w = 
+    let a = read_int () in
+    let i = read_int () in
+    if a < i then show (a, C i) else assert false in
+  let (b,js) = w in
+  match js with C j -> assert (b < j)
 
 
 
