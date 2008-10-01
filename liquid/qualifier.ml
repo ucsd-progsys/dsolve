de082690a7d3aaf101cbe07956870c7343c2b721
Pat Rondon <pmr@meatwad.ucsd.edu>
2008-10-01 10:23:33 -0700

    Useless exception, inaccurate comment

--- liquid/qualifier.ml
+++ liquid/qualifier.ml
@@ -34,8 +34,6 @@ let pprint ppf (_, _, pred) = Predicate.pprint ppf pred
 
 let apply x (_, y, p) = Predicate.subst x y p
 
-exception Refinement_not_closed
-
 (* The user specifies qualifiers as open predicates - i.e., the variables named
    in the qualifier may not yet be in scope at the time of definition.  But
    we want qualifiers that refer to OCaml paths, which are unique, not
@@ -47,9 +45,8 @@ exception Refinement_not_closed
    let a = 3 in ...   (* x's type is now incorrect *)
 
    This function replaces all instances of named variables in a qualifier with
-   the unique paths of the same name in the given environment.  It raises
-   Refinement_not_closed if a variable in the qualifier is not found in the
-   environment. *)
+   the unique paths of the same name in the given environment.  It returns
+   None if a variable in the qualifier is not found in the environment. *)
 let instantiate varmap (path, valu, pred) =
   (* Don't instantiate the bound variable *)
   let varmap = (Path.ident_name_crash valu, valu) :: varmap in
