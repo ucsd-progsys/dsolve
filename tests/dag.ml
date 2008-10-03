e496acfb251cc49306994631f4b57b18f0804fc4
Ranjit Jhala <rjhala@pho.(none)>
2008-10-03 12:54:23 -0700

    bizarre test tuplist.ml

--- tests/dag.ml
+++ tests/dag.ml
@@ -1,3 +1,28 @@
+let foo a b = 
+  (a,[])::b
+
+let _ = foo
+
+(*
+let _ = 
+  let n0 = 0 in
+  let l0 = [] in
+  let l1 = foo n0 l0 in
+  let n1 = n0 + 1 in
+  let l2 = foo n2 l2
+*)
+
+let rec build n xs = 
+  if read_int () > 0 then (n, xs) else
+    let n'  = n + 1  in
+    let xs' = foo n xs in
+    build n' xs'
+
+let _ = build 0 []
+
+
+let show x = x
+
 let flap f xs   = List.flatten (List.map f xs)
 
 (**************************************************************)
@@ -19,7 +44,7 @@ let check_node g n =
   | _ -> ()
 
 (* API *)
-let new_node g n = 
+let new_node n g = 
   (n,[])::g
 
 (* API *)
@@ -39,14 +64,15 @@ let succs g n =
 
 (* API *)
 let choose_node g = 
-  let n = List.fold_left (fun a (k,_) -> if read_int () > 1 then Some k else a) None g  in
+  let n = List.fold_left (fun a b -> if read_int () > 1 then Some (fst b) else a) None g  in
   match n with None -> assert false | Some n -> n
 
 (* API *)
-let check_dag (g, n) = 
+let check_dag n g = 
   List.iter 
-    (fun (i,js) -> 
-      let _ = assert (i <= n) in
+    (fun x -> 
+      let (i,js) = x in
+      let _      = assert (i <= n) in
       List.iter (fun j -> assert (i < j && j <= n)) js) 
     g
 
@@ -60,23 +86,38 @@ let rec leaves g n =
   | [] -> [n] 
   | ns -> flap (leaves g) ns
 
-let rec build_dag (g, n) =
-  let _ = check_dag (g, n) in
-  match read_int () with 
-  | 0 -> 
-      (g, n)
-  | 1 ->
-      let n' = fresh n in
-      build_dag (new_node g n', n') 
-  | _ -> 
-      let n1s = leaves g (choose_node g) in
-      let n2s = leaves g (choose_node g) in
-      let ns  = n1s @ n2s in
-      let n'  = fresh n in
-      let g'  = new_node g n' in 
-      let g'' = List.fold_left (fun g m -> new_edge g m n') g' ns in
-      build_dag (g'', n')
+(* 
+let rec build_dag n g =
+  let _ = check_dag n g in
+  let t = read_int () in
+  if t = 0 then 
+    (n, g)
+  else if t = 1 then
+    let n' = fresh n in
+    build_dag n' (new_node n' g) 
+  else
+    let n1s = leaves g (choose_node g) in
+    let n2s = leaves g (choose_node g) in
+    let ns  = n1s @ n2s in
+    let n'  = fresh n in
+    let g'  = new_node n' g in 
+    let g'' = List.fold_left (fun g m -> new_edge g m n') g' ns in
+    build_dag n' g''
 
-let _ = build_dag ([],0)
+let _ = build_dag 0 []
 
+*)
+    
 
+let _ =
+  let n0 = 0 in
+  let g0 = [] in
+  let n1 = fresh n0 in
+  let g1 = new_node n1 g0 in
+  show g1
+  (*
+  let _  = check_dag n1 g1 in
+  let n2 = fresh n1 in
+  let g2 = new_node n2 g1 in
+  check_dag n2 g2
+*)
