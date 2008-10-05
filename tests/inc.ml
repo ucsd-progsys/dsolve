77cb254423d709d9d0565bb2fffdeb9b139c76bc
Ranjit Jhala <rjhala@pho.(none)>
2008-10-05 11:51:09 -0700

    aa

--- tests/inc.ml
+++ tests/inc.ml
@@ -1,10 +1,13 @@
 let show x  = x
 let fresh x = x + 1
+
 let n0 = 0                    
 let n1 = fresh n0             
 let n2 = fresh n1
+
 let _  = show n0
 let _  = show n1
 let _  = show n2
+
 let _  = assert (n0 <= n1)
 let _  = assert (n1 <= n2)
