1f9e7998a79e941d8253c283f8c9f619a7d5c044
ming <mwookawa@wideload.(none)>
2008-10-14 17:08:49 -0700

    observation: when declared as recursive, an abstraction is done so that functions appear as if they've been called as far as our uncalled function finder is concerned

--- tests/rec/bad-as-length.ml
+++ tests/rec/bad-as-length.ml
@@ -2,7 +2,7 @@ let rec length x = match x with
   | []      -> 0
   | _ :: xs -> 1 + length xs
 
-let rec testlength xs = match xs with
+let testlength xs = match xs with
   | x :: y :: zs ->
       let s = length zs in
       assert (length xs >= 3)
--- tests/rec/bad-tuple-length.ml
+++ tests/rec/bad-tuple-length.ml
@@ -2,7 +2,7 @@ let rec length x = match x with
   | []      -> 0
   | _ :: xs -> 1 + length xs
 
-let rec testlength xs = match (xs, 3) with
+let testlength xs = match (xs, 3) with
   | (x :: y :: zs, _) ->
       let s = length zs in
       assert (length xs >= 3)
