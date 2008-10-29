d68c62d95475d39bd236a49b374ef4546b5286d5
Ranjit Jhala <rjhala@pho.(none)>
2008-10-28 22:10:28 -0700

    unionfind-simp now works (i.e. unionfind without path compression).

--- tests/unionfind-simp.ml
+++ tests/unionfind-simp.ml
@@ -1,6 +1,6 @@
 let show x = x
 
-(*let init_rank n = 
+let init_rank n = 
   let rec init_rec m i =
     if i >= n then m else init_rec (Store.set m i 1) (i+1) in
   init_rec Store.empty 0
@@ -11,98 +11,60 @@ let init_parent n =
   init_rec Store.empty 0
 
 let create n = 
-  (init_rank n, init_parent n)*)
+  (init_rank n, init_parent n)
 
-let rec find_aux (r: (int, int) Store.t) (p: (int, int) Store.t) (i: int) = 
+let rec find_aux (p: (int, int) Store.t) (i: int) = 
   let pi = Store.get p i in
   if pi = i then 
-    (p, i)
+    i
   else
-    let (p', i') = find_aux r p pi in 
-    (*let p''      = Store.set p' i i' in*)
-    (p'(*'*), i')
+    let i' = find_aux p pi in 
+    i'
 
-let find (r: (int, int) Store.t) (p: (int, int) Store.t) (i: int) =
-  find_aux r p i 
+let find (p: (int, int) Store.t) (i: int) =
+  find_aux p i 
   
-let union (h: (int, int) Store.t * (int, int) Store.t) (x: int) (y: int) =
-  let (r, p)   = h         in
-  let (p', x') = find r p x in
-  let (p'',y') = find r p' y in
-  let _ = assert (Store.get p' x' = Store.get p'' x') in
+(*let union (h: ((int, int) Store.t * (int, int) Store.t)) (x: int) (y: int) =
+  let (r, p)    = h in*)
+
+let union (r: (int, int) Store.t) (p: (int, int) Store.t) (x: int) (y: int) =
+  let x'        = find p x in
+  let y'        = find p y in
+  let _         = assert (Store.get p x' = x') in
+  let _         = assert (Store.get p y' = y') in
   if x' != y' then begin
     let rx' = Store.get r x' in
     let ry' = Store.get r y' in
     if rx' > ry' then
-      (*r, Store.set p'' y' x'*) assert false
+      (r, Store.set p y' x') 
     else if rx' < ry' then
-      (*r, Store.set p'' x' y'*) assert false
+      (r, Store.set p x' y') 
     else 
       let r' = Store.set r x' (rx' + 1) in
-      let _ = show x' in
-      (* let b = Store.get r' x' > Store.get r' y' in
-         let c = show b in*) 
-      let p''' = Store.set p'' y' x' in
-      (r', p''') (r, p)
+      let p' = Store.set p y' x' in
+      (r', p') 
   end else
-    (*r, p''*) assert false
+    (r, p) 
 
-(*************************************************)
-(*
-let check h = 
-  let (r,p) = h in
+
+(************************************************)
+
+let check r p = 
   Store.iter p (fun i v -> assert (v=i || Store.get r i < Store.get r v))
 
-let rec tester h = 
-  if read_int () > 0 then h else 
-    let x  = read_int () in
-    let y  = read_int () in
-    let h' = union h x y in
-    tester h
+let rec tester r p = 
+  if read_int () > 0 then (r, p) else 
+    let x       = read_int () in
+    let y       = read_int () in
+    let (r',p') = union r p x y in
+    tester r' p'
 
-
-let n  = read_int ()
-let h0 = create n 
-let h  = tester h0
-let _  = check h*)
-
-(*
-(* Union-Find using Tarjan's algorithm by J.C. Fillaitre *)
-type t = { 
-  mutable father: Store.t; (* mutable to allow path compression *)
-  c: Store.t;              (* ranks *)
-}
-
-let create n = 
-  { c = Store.create n 0;
-    father = Store.init n (fun i -> i) }
-    
-let rec find_aux f i = 
-  let fi = Store.get f i in
-  if fi == i then 
-    f, i
-  else 
-    let f, r = find_aux f fi in 
-    let f = Store.set f i r in
-    f, r
-      
-let find h x = 
-  let f,rx = find_aux h.father x in 
-  h.father <- f; rx
-  
-let union h x y = 
-  let rx = find h x in
-  let ry = find h y in
-  if rx != ry then begin
-    let rxc = Store.get h.c rx in
-    let ryc = Store.get h.c ry in
-    if rxc > ryc then
-      { h with father = Store.set h.father ry rx }
-    else if rxc < ryc then
-      { h with father = Store.set h.father rx ry }
-    else
-      { c = Store.set h.c rx (rxc + 1);
-	father = Store.set h.father ry rx }
-  end else
-    h
-*)
+let _ = 
+  let n         = read_int () in
+  let r         = init_rank n in
+  let p         = init_parent n in
+  let _         = show r in
+  let _         = show p in
+  let _         = check r p in
+  let (r', p')  = tester r p in 
+  check r' p'
