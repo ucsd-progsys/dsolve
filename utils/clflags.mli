ffdf9bff9e0b228f106d9e0a10a280c61d0b846e
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2008-10-29 21:37:55 -0700

    add -check-mlq option to do some type and scope checking over local mlq val statements

--- utils/clflags.mli
+++ utils/clflags.mli
@@ -80,6 +80,7 @@ val psimple: bool ref
 val simpguard : bool ref
 val no_recrefs: bool ref
 val no_recvarrefs: bool ref
+val ck_mlq: bool ref
 val no_simple_subs: bool ref
 val builtins_file: string option ref
 val less_qualifs: bool ref
