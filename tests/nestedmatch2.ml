017f6def0cff99ebfb53ca143cf5e0f6882ec0ad
Ranjit Jhala <rjhala@pho.(none)>
2008-10-31 10:56:18 -0700

    minimized tests/nestedmatch2.ml

--- tests/nestedmatch2.ml
+++ tests/nestedmatch2.ml
@@ -1,15 +1,11 @@
 type var = string
+
 type indexedvar = var * int * int 
+
 type constr = 
-  | Bottom of indexedvar	                (* x[i:j] = \bot *)
-  | IndexedEq of indexedvar * indexedvar        (* x[i:j] = x'[i':j'] *)
-  | IndexedLeq of indexedvar * indexedvar       (* x[i:j] <= x'[i':j'] *)
+  | Bottom of var * int * int   (* x[i:j] = \bot *)
 
 let refine b g t c = 
   match c with
   | Bottom (x,i,j) -> 
       (b, g, t) 
-  | IndexedEq (z,z') ->
-      (b, g, t)
-  | IndexedLeq (z,z') ->
-      (b, g, t)
