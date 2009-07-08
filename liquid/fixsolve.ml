7e339f7316a69d10fe063748be1c1989b8882baa
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2009-07-07 17:55:11 -0700

    stop passing quals to fixpoint

--- liquid/fixsolve.ml
+++ liquid/fixsolve.ml
@@ -17,10 +17,10 @@ let solver max_env cs soln qs =
   let fcs = Cg.f_of_dsubcons fmax_env cs in
   let fwfs = Cg.f_of_dwfcons cs in
   let soln = Cg.f_of_dsoln soln in
-  let fqs = Cg.f_of_dquals qs in
+  (*let fqs = Cg.f_of_dquals qs in*)
   let _ = Format.printf "@[InitSoln:@\n%a@]" FCo.print_soln soln in
   (* solve with fixpoint *)
-  let (solver, _) = Solve.create [] fsort_max_env [] fcs fwfs fqs in
+  let (solver, _) = Solve.create [] fsort_max_env [] fcs fwfs [] in
   let _ = Solve.save "/tmp/fix.in.fq" solver soln in
   let (soln, _) = Solve.solve solver soln in
   let _ = Format.printf "@[FinSoln:@\n%a@]" FCo.print_soln soln in
