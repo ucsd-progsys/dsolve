5f547165ad13241ea78deb2f8d3e706eb3693cdd
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2010-03-13 22:19:58 -0800

    generalize measure qualifiers

--- postests/incref.ml
+++ postests/incref.ml
@@ -4,6 +4,10 @@ let w = ref 0
 let _ = w <~ !w + 1
 let _ = assert (!w >= 0)
 
+let nonnull = function 
+  | x::xs -> true 
+  | []    -> (not (true))
+
 (* This fails 
 let y = ref 0
 let _ = y := !y
