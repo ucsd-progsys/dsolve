f2aa348c2433bf1678a7de9b6b1862610fc87fb4
Ranjit Jhala <rjhala@pho.ucsd.edu>
2010-03-13 19:04:38 -0800

    twiddle

--- postests/incref.ml
+++ postests/incref.ml
@@ -4,24 +4,8 @@ let w = ref 0
 let _ = w <~ !w + 1
 let _ = assert (!w >= 0)
 
-(* This fails 
+(* This fails *)
 let y = ref 0
 let _ = y := !y
 let _ = assert (!y >= 0)
-*)
-
-
-let rec ffor i j f = 
-  if i < j then begin
-    f i;
-    ffor (i+1) j f
-  end
-
-let min_index a =
-  let min = ref 0 in
-  ffor 0 (Array.length a) begin fun i ->
-    if a.(i) < a.(!min) then 
-      min <~ i
-  end;
-  !min
 
