937276fab1c4742290830ea512c32f17b741cad0
Ranjit Jhala <rjhala@pho.ucsd.edu>
2010-03-13 18:43:36 -0800

    some tweaks to the failing test

--- postests/incref.ml
+++ postests/incref.ml
@@ -1,4 +1,21 @@
-
+(* This works *)
 let x = ref 0
-let _ = x := !x (* comment this out and it works *) 
 let _ = assert (!x >= 0)
+
+(* This works *)
+let asgn = (:=)
+let z = ref 0
+let _ = asgn z !z
+let _ = assert (!z >= 0)
+
+(* This fails *) 
+let y = ref 0
+let _ = y := !y
+let _ = assert (!y >= 0)
+
+(* This fails *)
+let (<~) = (:=)
+let w = ref 0
+let _ = asgn w !w
+let _ = assert (!w >= 0)
+
