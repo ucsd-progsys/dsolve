84c22e5a5bb93f04a94d09663cace9faa0ebd58e
Ranjit Jhala <rjhala@pho.ucsd.edu>
2010-03-13 21:17:56 -0800

    twiddling the test...

--- postests/bindpat.ml
+++ postests/bindpat.ml
@@ -1,8 +1,12 @@
-(* This works *)
+(* (* This works *)
 let foo (x,y) = x + y
-let z = foo (2, 3)
-let _ = assert (z > 0)
+let z         = foo (2, 3)
+let _         = assert (z > 0)
+let _         = List.map (fun p -> let x,y = p in x+y) [(2,3)]
+*)
 
 (* But this crashes *)
-let _ = List.map foo [(2,3)]
+let bar (x,y) = x + y
+let _         = List.map bar [(2,3)]
+
 
