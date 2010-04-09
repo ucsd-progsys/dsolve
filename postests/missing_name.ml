142ccb349abf6ab7da75ccface99ffb9586c3fbd
Pat Rondon <pmr@plague>
2010-04-09 11:29:59 -0700

    churn

--- postests/missing_name.ml
+++ postests/missing_name.ml
@@ -4,15 +4,11 @@ type dict =
   | Red of dict * dict
 
 let restore_right arg = match arg with
-  | Black (lt, Red (rl, rr)) -> Black(Red(lt, rl), Empty)
-  | d -> assert false
+  | Black (lt, Red (rl, _)) -> Black(Red(lt, rl), Empty)
+  | _                       -> assert false
 
 let rec ins1 d = match d with
   | Black(left, right) -> restore_right (Black (left, ins1 right))
   | _                  -> Red (Empty, Empty)
 
-let dict = ins1 (Black (Empty, Empty))
-let _ =
-  match dict with
-    | Red (_, rt) -> rt
-    | dict        -> assert false
+let _ = ins1 (Black (Empty, Empty))
