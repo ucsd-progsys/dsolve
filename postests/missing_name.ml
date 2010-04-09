a06b24f22bcba9ebad793e17646dd3024aed1e2f
Pat Rondon <pmr@plague>
2010-04-09 10:30:14 -0700

    simplify test some more

--- postests/missing_name.ml
+++ postests/missing_name.ml
@@ -1,20 +1,11 @@
-(* 
-   Based on an example of Rowan Davies and Frank Pfenning and Joshua Dunfield
-*)
-
 type 'a dict =
     Empty
   | Black of 'a * 'a dict * 'a dict
   | Red of 'a * 'a dict * 'a dict
 
 let restore_right arg = match arg with
-  | Black (e, lt, Red (re, rl, rr)) ->
-      begin match (rl, rr) with
-        | (Red _, _) -> assert false
-        | (_, Red _) -> Black(re, Red(e, lt, rl), rr)
-        | _          -> arg
-      end
-  | d -> d
+  | Black (e, lt, Red (re, rl, rr)) -> Black(re, Red(e, lt, rl), rr)
+  | d -> assert false
 
 let rec ins1 key d = match d with
   | Black(key1, left, right) -> restore_right (Black (key1, left, ins1 key right))
@@ -24,12 +15,8 @@ let rec ins1 key d = match d with
 let insert dict key =
   let dict = ins1 key dict in
     match dict with
-      | Red (d, lt, rt) ->
-          begin match lt with
-            | Red _ -> Black (d, lt, rt) (* re-color *)
-            | _     -> assert false
-          end
-      | dict -> dict
+      | Red (d, lt, rt) -> Black (d, lt, rt)
+      | dict -> assert false
 
 let test_insert x x' =
   let a = insert Empty x in
