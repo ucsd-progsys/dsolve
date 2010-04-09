83a26486bd507024b1aae037302ea1380fb3e35f
Pat Rondon <pmr@plague>
2010-04-09 10:52:58 -0700

    le sigh

--- postests/missing_name.ml
+++ postests/missing_name.ml
@@ -1,24 +1,24 @@
-type 'a dict =
+type dict =
     Empty
-  | Black of 'a * 'a dict * 'a dict
-  | Red of 'a * 'a dict * 'a dict
+  | Black of dict * dict
+  | Red of dict * dict
 
 let restore_right arg = match arg with
-  | Black (e, lt, Red (re, rl, rr)) -> Black(re, Red(e, lt, rl), rr)
+  | Black ( lt, Red (rl, rr)) -> Black(Red(lt, rl), rr)
   | d -> assert false
 
 let rec ins1 key d = match d with
-  | Black(key1, left, right) -> restore_right (Black (key1, left, ins1 key right))
-  | Red _                    -> assert false
-  | Empty                    -> Red (key, Empty, Empty)
+  | Black(left, right) -> restore_right (Black (left, ins1 key right))
+  | Red _              -> assert false
+  | Empty              -> Red (Empty, Empty)
 
 let insert dict key =
   let dict = ins1 key dict in
     match dict with
-      | Red (d, lt, rt) -> Black (d, lt, rt)
+      | Red (lt, rt) -> Black (Empty, rt)
       | dict -> assert false
 
 let test_insert x x' =
-  let a = insert Empty x in
+  let a = Black (Empty, Empty) in
   let b = insert a x' in
     ()
