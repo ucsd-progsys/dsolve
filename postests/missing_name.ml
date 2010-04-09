20ce49862ec55cd53f2367305177ce5eb1cea5e2
Pat Rondon <pmr@plague>
2010-04-09 13:29:58 -0700

    tweak

--- postests/missing_name.ml
+++ postests/missing_name.ml
@@ -4,11 +4,11 @@ type dict =
   | Red of dict * dict
 
 let restore_right arg = match arg with
-  | Black (lt, Red (rl, _)) -> Black(Red(lt, rl), Empty)
+  | Black (lt, Red (rl, _)) -> Black (Red (lt, rl), Empty)
   | _                       -> assert false
 
 let rec ins1 d = match d with
-  | Black(left, right) -> restore_right (Black (left, ins1 right))
-  | _                  -> Red (Empty, Empty)
+  | Black (_, right) -> restore_right (Black (Empty, ins1 right))
+  | _                -> Red (Empty, Empty)
 
 let _ = ins1 (Black (Empty, Empty))
