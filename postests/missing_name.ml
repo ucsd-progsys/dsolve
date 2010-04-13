d1313ffa4c56bd143f3be3b3daba73bd5a8415e8
Pat Rondon <pmr@famine>
2010-04-13 12:02:15 -0700

    same bug, fewer quals

--- postests/missing_name.ml
+++ postests/missing_name.ml
@@ -5,7 +5,7 @@ type dict =
 
 let rec ins d = match d with
   | Black (_, right) ->
-      begin match (Black (Empty, ins right)) with
+      begin match Black (Empty, ins right) with
         | Black (lt, Red (rl, _)) -> Black (Red (lt, rl), Empty)
       end
   | _ -> Red (Empty, Empty)
