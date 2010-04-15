9014e87d854a1511c0b71198687d6c6fa97571a4
Pat Rondon <pmr@famine>
2010-04-15 15:15:40 -0700

    only needs one type constructor now!

--- postests/missing_name.ml
+++ postests/missing_name.ml
@@ -3,13 +3,12 @@
 type dict =
     Empty
   | Black of dict * dict
-  | Red of dict * dict
 
 let rec ins d =
   if d then
     match Black (Empty, ins false) with
-      | Black (_, Red (rl, lt)) -> Red (lt, rl)
+      | Black (_, Black (rl, lt)) -> Black (lt, rl)
   else
-    Red (Empty, Empty)
+    Black (Empty, Empty)
 
 let _ = ins true
