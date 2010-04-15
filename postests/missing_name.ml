601647594601034058e984ef9100c15f0506a2c9
Pat Rondon <pmr@famine>
2010-04-15 16:59:04 -0700

    revert changes to missing_name :'(

--- postests/missing_name.ml
+++ postests/missing_name.ml
@@ -3,8 +3,13 @@
 type dict =
     Empty
   | Black of dict * dict
+  | Red of dict * dict
 
-let _ =
-  match Black (Empty, Black (Empty, Empty)) with
-    | Black (_, Black (y, z)) -> y
-(* But | Black (x, Black (y, z)) -> y works! *)
+let rec ins d =
+  if d then
+    match Black (Empty, ins false) with
+      | Black (lt, Red (rl, _)) -> Black (Red (lt, rl), Empty)
+  else
+    Red (Empty, Empty)
+
+let _ = ins true
