30d34a7bcca2e480e454412d635593565dbe558e
Ranjit Jhala <jhala@ubuntu.(none)>
2010-05-15 09:30:45 -0700

    rejiggering getsubmodules...

--- getsubmodules.sh
+++ getsubmodules.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
+if $1
+then
+  prefix="meatwad.ucsd.edu:/git/"
+else
+  prefix=$1"@meatwad.ucsd.edu:/git/"
+fi
+
+z3=$prefix"external/z3"
+fp=$prefix"external/fixpoint"
+og=$prefix"external/ocamlgraph"
+mi=$prefix"external/misc"
+
+echo $z3
+echo $fp
+echo $og
+echo $mi
+
 cd external
-git clone meatwad.ucsd.edu:/git/external/z3
-git clone meatwad.ucsd.edu:/git/external/fixpoint
-git clone meatwad.ucsd.edu:/git/external/ocamlgraph
-git clone meatwad.ucsd.edu:/git/external/misc
+git clone "$z3"
+git clone "$fp"
+git clone "$og"
+git clone "$mi"
