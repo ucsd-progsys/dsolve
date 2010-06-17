ad7226bc6e71899ad14661294f8a08df22439698
Ranjit Jhala <rjhala@pho.ucsd.edu>
2010-06-17 14:29:10 -0700

    what the fuck!

--- postests/POPL2010/max.ml
+++ postests/POPL2010/max.ml
@@ -1,18 +1,16 @@
 let max2 (x:int) (y:int) = if x >= y then x else y
-
-let mmax (x:int) (y:int) (z:int) = max2 (max2 x y) z
-
 let x  = read_int ()
 let y  = read_int ()
-let z  = read_int ()
+let m2 = max2 x y
 
-let m2 = mmax x y z
 let _  = assert (x <= m2)
+
+(* 
+let mmax (x:int) (y:int) (z:int) = max2 x y 
+let z  = read_int ()
 let _  = assert (y <= m2)
-let _  = assert (z <= m2)
+let _  = assert (z <= m2) 
 
-
-(*
 let max3 f (x:int) (y:int) (z:int) = f (f x y) z
 
 let m1 = max3 max2 x y z
