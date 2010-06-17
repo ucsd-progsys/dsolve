081597f187103f39d157e3324effceddffb478e6
Ranjit Jhala <rjhala@pho.ucsd.edu>
2010-06-17 12:10:33 -0700

    fiddling with sum.ml

--- postests/POPL2010/mult.ml
+++ postests/POPL2010/mult.ml
@@ -1,2 +1,10 @@
-let rec mult x y = if (x <= 0 || y <= 0) then 0 else x + (mult x (y - 1))
-and main () = assert (100 <= mult 100 100)
+let rec mult x y = 
+  if (x <= 0 || y <= 0) 
+  then 0 
+  else (x + (mult x (y - 1)))
+
+(*
+let _  = assert (100 <= mult 100 100)
+*)
+
+let main n = if 0 <= n then assert (n <= mult n n) 
--- postests/POPL2010/sum.ml
+++ postests/POPL2010/sum.ml
@@ -1,2 +1,7 @@
 let rec sum x = if x <= 0 then 0 else x + sum (x - 1)
-and main m = assert (m <= sum m)
+
+let m = read_int ()
+let _ = assert (m <= sum m)
+
+let main m = assert (m <= sum m)
+
