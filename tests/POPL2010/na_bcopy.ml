f9693fc53694a7928e4d9b4d45fe1dd258390996
Ranjit Jhala <rjhala@pho.ucsd.edu>
2010-06-18 15:40:26 -0700

    a simple test that tickles the z3Rel bug.

--- tests/POPL2010/na_bcopy.ml
+++ tests/POPL2010/na_bcopy.ml
@@ -2,12 +2,12 @@ let rec bcopy_aux src des i m =
   if i = m then 
     () 
   else begin
-    Junkarray.set des i (Junkarray.get src i);
+    Junkarray2.set des i (Junkarray2.get src i);
     bcopy_aux src des (i+1) m
   end
 
 let bcopy src des =
-  let sz = Junkarray.length src in
+  let sz = Junkarray2.length src in
   bcopy_aux src des 0 sz
 
 (********************* Driver **********************)
@@ -15,6 +15,6 @@ let bcopy src des =
 let _ = 
   let sz = read_int () in 
   if 0 < sz then
-    let src = Junkarray.init sz (fun i -> i) in
-    let dst = Junkarray.make sz 0 in
+    let src = Junkarray2.init sz (fun i -> i) in
+    let dst = Junkarray2.make sz 0 in
     bcopy src dst
