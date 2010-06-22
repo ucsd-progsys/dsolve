d0921e8be12b7c3950355c2bf4d6efc2b0dd1974
Pat Rondon <prondon@cs.ucsd.edu>
2010-06-22 14:55:51 -0700

    ruslan's newest creation

---
+++ postests/ruslan-1.ml
@@ -0,0 +1,13 @@
+let rec mlength l =
+  match l with
+    | [] -> 0
+    | _ :: t -> 1 + mlength t
+
+let test2 l2_1 x =
+  match l2_1 with
+    | [] ->
+        assert (0 = (mlength l2_1) * (mlength l2_1));
+        assert (0 = (mlength l2_1) * (mlength l2_1));
+        0
+    | h :: t ->
+        0
