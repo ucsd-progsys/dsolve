271aedbea496cdb2bf32a717e815c418f518b90e
Ranjit Jhala <jhala@ubuntu.(none)>
2010-06-25 18:36:50 -0700

    foo

--- tests/POPL2010/na_heapsort-e.ml
+++ tests/POPL2010/na_heapsort-e.ml
@@ -5,23 +5,23 @@ let rec heapify size data i =
   let right = right + 2 in
   let largest = 
     if left < size then
-      let gl = Array.get data left in
-      let gi = Array.get data i in
+      let gl = Junkarray2.get data left in
+      let gi = Junkarray2.get data i in
       if gl > gi then left else i 
     else i
   in
   let largest =
     if right < size then
-      let gr = Array.get data right in
-      let gx = Array.get data largest in 
+      let gr = Junkarray2.get data right in
+      let gx = Junkarray2.get data largest in 
       if gr > gx then right else largest
     else largest
   in
   if largest > i then
-    let temp  = Array.get data i in
-    let temp2 = Array.get data largest in
-    let _     = Array.set data i temp2 in
-    let _     = Array.set data largest temp in
+    let temp  = Junkarray2.get data i in
+    let temp2 = Junkarray2.get data largest in
+    let _     = Junkarray2.set data i temp2 in
+    let _     = Junkarray2.set data largest temp in
     heapify size data largest
   else ()
 
@@ -37,10 +37,10 @@ let heapsort maxx size data =
   let  _ = buildheap size data in
   let rec loop i =
     if i >= 1 then
-      let temp = Array.get data i in
-      let gd0  = Array.get data 0 in
-      let _    = Array.set data i gd0 in
-      let _    = Array.set data 0 temp in
+      let temp = Junkarray2.get data i in
+      let gd0  = Junkarray2.get data 0 in
+      let _    = Junkarray2.set data i gd0 in
+      let _    = Junkarray2.set data 0 temp in
       let _    = heapify i data 0 in
       loop (i - 1) 
     else ()
@@ -49,7 +49,7 @@ let heapsort maxx size data =
 let print_array	data i j =
   let rec loop k =
     if k <= j then
-      let sdk = Array.get data k in
+      let sdk = Junkarray2.get data k in
       let _   = print_float sdk in
       loop (k+1) 
     else ()
@@ -58,12 +58,12 @@ let print_array	data i j =
 
 let driver =
   let maxx = 16 in
-  let data = Array.make maxx 0.0 in
+  let data = Junkarray2.make maxx 0.0 in
   for i = 0 to maxx - 1 do 
     let isq  = i * i in
     let i_16 = 16 * i in 
     let diff = i_16 - isq in
-    Array.set data i (float_of_int diff) 
+    Junkarray2.set data i (float_of_int diff) 
   done; 
   print_string "before: "; 
   print_array data 0 maxx;
