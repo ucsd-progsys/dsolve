35d9281d44ea86536562d2a62734fd6ec5bdf4f5
Pat Rondon <prondon@cs.ucsd.edu>
2010-06-29 10:27:02 -0700

    simplify f's type

--- negtests/contributed/bug.004.ml
+++ negtests/contributed/bug.004.ml
@@ -3,7 +3,7 @@
 (* If r_2 is a function, this works ok.
  * If r_2 is a value, f gets a bogus type.
  *)
-let rec (f, r_2) = (fun x -> x), 0
+let rec (f, r_2) = (fun (x: int) -> x), 0
 
 let _ = assert (f 3 = 10000)
 
