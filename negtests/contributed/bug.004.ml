82b17cdb20885cb6e541fa9a56658809d14e7c87
Pat Rondon <prondon@cs.ucsd.edu>
2010-06-29 10:31:13 -0700

    change to let rec, simplifies a little

--- negtests/contributed/bug.004.ml
+++ negtests/contributed/bug.004.ml
@@ -3,7 +3,8 @@
 (* If r_2 is a function, this works ok.
  * If r_2 is a value, f gets a bogus type.
  *)
-let rec (f, r_2) = (fun (x: int) -> x), 0
+let rec f (x: int) = x
+and r_2 = 0
 
 let _ = assert (f 3 = 10000)
 
