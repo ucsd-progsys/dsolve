8c27aac2f6d61b63e1d27afa5b0501ca0adb3a8b
Pat Rondon <prondon@cs.ucsd.edu>
2010-06-30 19:06:05 -0700

    push vecswap through with bare

--- tests/POPL2008/vecswap.ml
+++ tests/POPL2008/vecswap.ml
@@ -1,21 +1,23 @@
+(* DSOLVE -bare -dontminemlq *)
+
 let min a b = if a < b then a else b 
 
-let rec sortRange arr start n =
-    let item i = Array.get arr i in
-    let swap i j =
-     let tmp = item i in
-      (Array.set arr i (item j); Array.set arr j tmp)
-     in
-    let rec vecswap i j n =
-      if n = 0 then () else (swap i j; vecswap (i+1) (j+1) (n-1))
-    in
-    let y = Random.int n in 
-    let z = Random.int n in
-    let x = Random.int n in
-      if y + z <= n then
-        if z + x <= n then
-      vecswap x y z
-      else () else ()
+let sortRange arr start n =
+  let item i = Array.get arr i in
+  let swap i j =
+   let tmp = item i in
+    (Array.set arr i (item j); Array.set arr j tmp)
+   in
+  let rec vecswap i j n =
+    if n = 0 then () else (swap i j; vecswap (i+1) (j+1) (n-1))
+  in
+  let y = Random.int n in
+  let z = Random.int n in
+  let x = Random.int n in
+    if y + z <= n then
+      if z + x <= n then
+    vecswap x y z
+    else () else ()
 
 let driver =
   let _n = Random.self_init () in
