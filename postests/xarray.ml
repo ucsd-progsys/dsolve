dd397d38224f7555952c0d32416b4339b3e76b7f
Pat Rondon <prondon@cs.ucsd.edu>
2010-07-04 15:15:20 -0700

    working on sort in xarray; I thought we did this??

--- postests/xarray.ml
+++ postests/xarray.ml
@@ -166,7 +166,6 @@ let fold_right f a x =
   done;
   !r
 
-(*
 type 'a fakebottom = Bottom of int | Ok of 'a
 
 let sort cmp a =
@@ -174,28 +173,28 @@ let sort cmp a =
     let i31 = i+i+i+1 in
     let x = ref i31 in
     if i31+2 < l then begin
-      if cmp (get a i31) (get a (i31+1)) < 0 then x := i31+1;
-      if cmp (get a !x) (get a (i31+2)) < 0 then x := i31+2;
+      if cmp (Array.get a i31) (Array.get a (i31+1)) < 0 then x := i31+1;
+      if cmp (Array.get a !x) (Array.get a (i31+2)) < 0 then x := i31+2;
       Ok !x
     end else
-      if i31+1 < l && cmp (get a i31) (get a (i31+1)) < 0
+      if i31+1 < l && cmp (Array.get a i31) (Array.get a (i31+1)) < 0
       then Ok (i31+1)
       else if i31 < l then Ok i31 else Bottom i
   in
   let rec trickledown l i e =
     match maxson l i with Bottom i -> Bottom i | Ok j -> 
-      if cmp (get a j) e > 0 then begin
-        set a i (get a j);
+      if cmp (Array.get a j) e > 0 then begin
+        Array.set a i (Array.get a j);
         trickledown l j e
       end else begin
-        set a i e;
+        Array.set a i e;
         Ok ()
       end;
   in
-  let rec trickle l i e = match trickledown l i e with Ok _ -> () | Bottom i -> set a i e in
+  let rec trickle l i e = match trickledown l i e with Ok _ -> () | Bottom i -> Array.set a i e in
   let rec bubbledown l i =
     match maxson l i with Bottom i -> Bottom i | Ok j -> 
-    set a i (get a j);
+    Array.set a i (Array.get a j);
     bubbledown l j
   in
   let bubble l i = match bubbledown l i with Ok _ -> (assert (0=1); assert false) | Bottom i -> i in
@@ -204,24 +203,24 @@ let sort cmp a =
     let z = assert (i >= 0) in
     let z = assert (father >= 0) in
     assert (i <> father);
-    if cmp (get a father) e < 0 then begin
-      set a i (get a father);
-      if father > 0 then trickleup father e else set a 0 e;
+    if cmp (Array.get a father) e < 0 then begin
+      Array.set a i (Array.get a father);
+      if father > 0 then trickleup father e else Array.set a 0 e;
     end else begin
-      set a i e;
+      Array.set a i e;
     end;
   in
-  let l = length a in
-  for i = (l + 1) / 3 - 1 downto 0 do trickle l i (get a i); done;
+  let l = Array.length a in
+  for i = (l + 1) / 3 - 1 downto 0 do trickle l i (Array.get a i); done;
   for i = l - 1 downto 2 do
-    let e = (get a i) in
-    set a i (get a 0);
+    let e = (Array.get a i) in
+    Array.set a i (Array.get a 0);
     trickleup (bubble i 0) e;
   done;
-  if l > 1 then (let e = (get a 1) in set a 1 (get a 0); set a 0 e);
+  if l > 1 then (let e = (Array.get a 1) in Array.set a 1 (Array.get a 0); Array.set a 0 e);
 ;;
-*)
 
+(*
 let cutoff = 5;;
 let stable_sort cmp a =
   let merge src1ofs src1len src2 src2ofs src2len dst dstofs =
@@ -282,4 +281,4 @@ let stable_sort cmp a =
 ;;
 
 (* let fast_sort = stable_sort;; *)
-
+*)
