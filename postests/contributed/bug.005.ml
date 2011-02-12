73adae4d81731fee07f2f9e50e67e4b5300a7779
Pat Rondon <prondon@cs.ucsd.edu>
2011-02-12 11:04:47 -0800

    "fix" bug.005.ml

--- postests/contributed/bug.005.ml
+++ postests/contributed/bug.005.ml
@@ -13,7 +13,7 @@ let rec mlength x =
 
 let rec assoc x l1 y s =
   match l1 with
-    |  [] -> raise Not_found, s
+    |  [] -> (* raise Not_found, s *) assert false
     | (a,b)::l ->
         let x_16 = mlength l in
         if compare a x = 0 then (b, s) else assoc x l y (s + 1)
